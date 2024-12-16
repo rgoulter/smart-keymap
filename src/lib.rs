70bcfc56d494a8944bae863863c03c122757cced
Richard Goulter <richard.goulter@gmail.com>
2024-12-16 21:54:35 +0700

    rust: impl keymap for global functions

--- src/lib.rs
+++ src/lib.rs
@@ -3,16 +3,28 @@
 mod input;
 mod keymap;
 
+
+static mut KEYMAP: keymap::Keymap<3> = keymap::Keymap::new(keymap::KEY_DEFINITIONS);
+
 #[no_mangle]
 pub extern "C" fn keymap_init() {
+    unsafe {
+        KEYMAP.init();
+    }
 }
 
 #[no_mangle]
 pub extern "C" fn keymap_register_input_keypress(index: u16) {
+    unsafe {
+        KEYMAP.handle_input(input::Event::Press(index));
+    }
 }
 
 #[no_mangle]
 pub extern "C" fn keymap_register_input_keyrelease(index: u16) {
+    unsafe {
+        KEYMAP.handle_input(input::Event::Release(index));
+    }
 }
 
 #[no_mangle]
@@ -22,7 +34,8 @@ pub extern "C" fn copy_hid_boot_keyboard_report(buf: *mut u8) {
     }
 
     unsafe {
-        core::ptr::write_bytes(buf, 0, 8);
+        let report = KEYMAP.boot_keyboard_report();
+        core::ptr::copy_nonoverlapping(report.as_ptr(), buf, report.len());
     }
 }
 
