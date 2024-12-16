e19f288d7ecc695fe6f10e3b0455159cca312c2d
Richard Goulter <richard.goulter@gmail.com>
2024-12-16 20:32:43 +0700

    tests: C: add test for simple key press/release

--- tests/c_tests/test_keymap.c
+++ tests/c_tests/test_keymap.c
@@ -2,6 +2,8 @@
 
 #include "smart_keymap.h"
 
+#define KC_A 0x04
+
 void setUp(void) {
 }
 
@@ -17,10 +19,42 @@ void test_copy_hid_boot_keyboard_report(void) {
     TEST_ASSERT_EQUAL_UINT8_ARRAY(expected_report, actual_report, 8);
 }
 
+// KEYMAP: [A, A, A]
+
+void test_simple_keypress(void) {
+    uint8_t expected_report[8] = {0, 0, KC_A, 0, 0, 0, 0, 0};
+    uint8_t actual_report[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+
+    keymap_init();
+
+    keymap_register_input_keypress(2); // Third key in the keymap is A
+
+    copy_hid_boot_keyboard_report(actual_report);
+
+    TEST_ASSERT_EQUAL_UINT8_ARRAY(expected_report, actual_report, 8);
+}
+
+void test_simple_keyrelease(void) {
+    uint8_t expected_report[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+    uint8_t actual_report[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+
+    keymap_init();
+
+    keymap_register_input_keypress(2);
+    keymap_register_input_keyrelease(2);
+
+    copy_hid_boot_keyboard_report(actual_report);
+
+    TEST_ASSERT_EQUAL_UINT8_ARRAY(expected_report, actual_report, 8);
+}
+
 int main(void) {
     UNITY_BEGIN();
     
     RUN_TEST(test_copy_hid_boot_keyboard_report);
+
+    RUN_TEST(test_simple_keypress);
+    RUN_TEST(test_simple_keyrelease);
 
     return UNITY_END();
 }
