413f01793662f5465a8cb1a51d437a0cbe5c782e
Richard Goulter <richard.goulter@gmail.com>
2024-12-16 22:11:07 +0700

    Add simple key definition (#3)
    * rust: C: add functions to interface
    
    * tests: C: add test for simple key press/release
    
    * fixup! rust: C: add functions to interface
    
    * rust: add dependency: heapless
    
    * rust: implement basic Keymap
    
    * rust: impl keymap for global functions
    
    * rust: add another simple key to simple keymap
    
    * tests: C: add more test cases
    
    * ci: add bindgen

--- tests/c_tests/test_keymap.c
+++ tests/c_tests/test_keymap.c
@@ -2,6 +2,9 @@
 
 #include "smart_keymap.h"
 
+#define KC_A 0x04
+#define KC_B 0x05
+
 void setUp(void) {
 }
 
@@ -17,10 +20,109 @@ void test_copy_hid_boot_keyboard_report(void) {
     TEST_ASSERT_EQUAL_UINT8_ARRAY(expected_report, actual_report, 8);
 }
 
+// KEYMAP: [A, A, A, B]
+
+void test_simple_keypress(void) {
+    uint8_t expected_report[8] = {0, 0, KC_A, 0, 0, 0, 0, 0};
+    uint8_t actual_report[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+
+    keymap_init();
+
+    keymap_register_input_keypress(2); // Third key in the keymap is A
+
+    copy_hid_boot_keyboard_report(actual_report);
+
+    TEST_ASSERT_EQUAL_UINT8_ARRAY(expected_report, actual_report, 8);
+}
+
+void test_simple_keyrelease(void) {
+    uint8_t expected_report[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+    uint8_t actual_report[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+
+    keymap_init();
+
+    keymap_register_input_keypress(2);
+    keymap_register_input_keyrelease(2);
+
+    copy_hid_boot_keyboard_report(actual_report);
+
+    TEST_ASSERT_EQUAL_UINT8_ARRAY(expected_report, actual_report, 8);
+}
+
+void test_simple_keypress_sequence_da_db(void) {
+    // Pressing A, then B, should report "A B"
+
+    uint8_t expected_report[8] = {0, 0, KC_A, KC_B, 0, 0, 0, 0};
+    uint8_t actual_report[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+
+    keymap_init();
+
+    keymap_register_input_keypress(2); // Third key in the keymap is A
+    keymap_register_input_keypress(3); // Fourth key in the keymap is B
+
+    copy_hid_boot_keyboard_report(actual_report);
+    TEST_ASSERT_EQUAL_UINT8_ARRAY(expected_report, actual_report, 8);
+}
+
+void test_simple_keypress_sequence_db_da(void) {
+    // Pressing B, then A, should report "B A"
+
+    uint8_t expected_report[8] = {0, 0, KC_B, KC_A, 0, 0, 0, 0};
+    uint8_t actual_report[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+
+    keymap_init();
+
+    keymap_register_input_keypress(3); // Fourth key in the keymap is B
+    keymap_register_input_keypress(2); // Third key in the keymap is A
+
+    copy_hid_boot_keyboard_report(actual_report);
+    TEST_ASSERT_EQUAL_UINT8_ARRAY(expected_report, actual_report, 8);
+}
+
+void test_simple_keypress_sequence_da_db_ub(void) {
+    // Pressing A, then B; then releasing B, should report "A"
+
+    uint8_t expected_report[8] = {0, 0, KC_A, 0, 0, 0, 0, 0};
+    uint8_t actual_report[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+
+    keymap_init();
+
+    keymap_register_input_keypress(2); // Third key in the keymap is A
+    keymap_register_input_keypress(3); // Fourth key in the keymap is B
+    keymap_register_input_keyrelease(3);
+
+    copy_hid_boot_keyboard_report(actual_report);
+    TEST_ASSERT_EQUAL_UINT8_ARRAY(expected_report, actual_report, 8);
+}
+
+void test_simple_keypress_sequence_da_db_ua(void) {
+    // Pressing A, then B; then releasing A, should report "B"
+
+    uint8_t expected_report[8] = {0, 0, KC_B, 0, 0, 0, 0, 0};
+    uint8_t actual_report[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+
+    keymap_init();
+
+    keymap_register_input_keypress(2); // Third key in the keymap is A
+    keymap_register_input_keypress(3); // Fourth key in the keymap is B
+    keymap_register_input_keyrelease(2);
+
+    copy_hid_boot_keyboard_report(actual_report);
+    TEST_ASSERT_EQUAL_UINT8_ARRAY(expected_report, actual_report, 8);
+}
+
 int main(void) {
     UNITY_BEGIN();
     
     RUN_TEST(test_copy_hid_boot_keyboard_report);
+
+    RUN_TEST(test_simple_keypress);
+    RUN_TEST(test_simple_keyrelease);
+
+    RUN_TEST(test_simple_keypress_sequence_da_db);
+    RUN_TEST(test_simple_keypress_sequence_da_db_ua);
+    RUN_TEST(test_simple_keypress_sequence_da_db_ub);
+    RUN_TEST(test_simple_keypress_sequence_db_da);
 
     return UNITY_END();
 }
