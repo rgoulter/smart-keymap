796f2c307721c3c9e5aea1eb0dafc3bb7f4e4990
Richard Goulter <richard.goulter@gmail.com>
2024-12-18 19:21:35 +0700

    devenv: enable clippy, rustfmt

--- devenv.nix
+++ devenv.nix
@@ -1,6 +1,11 @@
 { pkgs, lib, config, inputs, ... }:
 
 {
+  pre-commit.hooks = {
+    clippy.enable = true;
+    rustfmt.enable = true;
+  };
+
   languages = {
     c.enable = true;
     ruby.enable = true;
