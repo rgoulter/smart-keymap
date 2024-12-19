759add9548deaf0024b74acd21393aa8c9f732d7
Richard Goulter <richard.goulter@gmail.com>
2024-12-19 11:02:16 +0700

    keydef: TapHold: implement TapHold eventually holds

--- src/input.rs
+++ src/input.rs
@@ -1,7 +1,8 @@
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq, PartialOrd, Eq, Ord)]
 pub enum Event {
     Press { keymap_index: u16 },
     Release { keymap_index: u16 },
     VirtualKeyPress { keycode: u8 },
     VirtualKeyRelease { keycode: u8 },
+    TapHoldTimeout { keymap_index: u16 },
 }
