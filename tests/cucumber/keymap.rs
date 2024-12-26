b6efb6a1396b0074a408f70c3ba4f8c60a8e0077
Richard Goulter <richard.goulter@gmail.com>
2024-12-26 18:22:04 +0700

    tests: cucumber: implement a keymap test for Simple

---
+++ tests/cucumber/keymap.rs
@@ -0,0 +1,69 @@
+use std::fmt::Debug;
+use std::fmt::Formatter;
+
+use cucumber::gherkin::Step;
+use cucumber::{given, then, when, World};
+
+use smart_keymap::input;
+use smart_keymap::key;
+use smart_keymap::keymap::Keymap;
+
+use key::composite::Key;
+
+mod common;
+
+use common::Deserializer;
+
+#[derive(Debug, World)]
+pub struct KeymapWorld {
+    input_deserializer: Deserializer,
+    keymap: Keymap<Vec<Key>>,
+}
+
+impl Default for KeymapWorld {
+    fn default() -> Self {
+        let keymap = Keymap::new(Vec::new(), key::composite::Context::new());
+        KeymapWorld {
+            input_deserializer: Deserializer::JSON,
+            keymap,
+        }
+    }
+}
+
+#[given(expr = "a keymap, expressed as a {deserializer} string")]
+fn setup_keymap(world: &mut KeymapWorld, step: &Step, deserializer: Deserializer) {
+    let keys: Vec<Key> = deserializer
+        .from_str(step.docstring().as_ref().unwrap())
+        .unwrap();
+
+    world.input_deserializer = deserializer;
+    world.keymap = Keymap::new(keys, key::composite::Context::new());
+}
+
+#[when("the keymap registers the following input")]
+fn perform_input(world: &mut KeymapWorld, step: &Step) {
+    let inputs: Vec<input::Event> = world
+        .input_deserializer
+        .from_str(step.docstring().as_ref().unwrap())
+        .unwrap();
+
+    for input in inputs {
+        world.keymap.handle_input(input);
+    }
+}
+
+#[then("the HID keyboard report should be")]
+fn check_report(world: &mut KeymapWorld, step: &Step) {
+    let expected_report: Vec<u8> = world
+        .input_deserializer
+        .from_str(step.docstring().as_ref().unwrap())
+        .unwrap();
+
+    let actual_report = world.keymap.boot_keyboard_report();
+
+    assert_eq!(expected_report, actual_report);
+}
+
+fn main() {
+    futures::executor::block_on(KeymapWorld::run("features/keymap/"));
+}
