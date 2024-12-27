97bc06cda7ba4aa0a97b54602e00c17f92ab5b71
Richard Goulter <richard.goulter@gmail.com>
2024-12-27 20:58:33 +0700

    rust: add LayerModifier to key::composite::Key

--- src/key/composite.rs
+++ src/key/composite.rs
@@ -10,21 +10,22 @@ use crate::key;
 use key::{layered, simple, tap_hold};
 
 #[derive(Deserialize, Debug, Clone, Copy, PartialEq)]
-pub enum Key {
+pub enum Key<const L: layered::LayerIndex = 0> {
     Simple(simple::Key),
     TapHold(tap_hold::Key),
+    LayerModifier(layered::ModifierKey<L>),
 }
 
-impl key::Key for Key {
+impl<const L: layered::LayerIndex> key::Key for Key<L> {
     type Context = Context;
     type Event = Event;
-    type PressedKey = PressedKey;
+    type PressedKey = PressedKey<L>;
 
     fn new_pressed_key(
         &self,
         _context: &Self::Context,
         keymap_index: u16,
-    ) -> (PressedKey, Option<key::ScheduledEvent<Event>>) {
+    ) -> (Self::PressedKey, Option<key::ScheduledEvent<Event>>) {
         match self {
             Key::Simple(k) => {
                 let pressed_key = simple::PressedKey::new(k.key_code());
@@ -32,6 +33,10 @@ impl key::Key for Key {
             }
             Key::TapHold(_) => {
                 let (pressed_key, new_event) = tap_hold::PressedKey::new(keymap_index);
+                (pressed_key.into(), Some(new_event.into()))
+            }
+            Key::LayerModifier(k) => {
+                let (pressed_key, new_event) = k.new_pressed_key(keymap_index);
                 (pressed_key.into(), Some(new_event.into()))
             }
         }
@@ -59,29 +64,41 @@ impl key::Context for Context {
 }
 
 #[derive(Debug, Clone, Copy)]
-pub enum PressedKey {
+pub enum PressedKey<const L: layered::LayerIndex = 0> {
     Simple(simple::PressedKey),
     TapHold(tap_hold::PressedKey),
+    LayerModifier(layered::PressedModifierKey<L>),
 }
 
-impl From<simple::PressedKey> for PressedKey {
+impl<const L: layered::LayerIndex> From<layered::PressedModifierKey<L>> for PressedKey<L> {
+    fn from(pk: layered::PressedModifierKey<L>) -> Self {
+        PressedKey::LayerModifier(pk)
+    }
+}
+
+impl<const L: layered::LayerIndex> From<simple::PressedKey> for PressedKey<L> {
     fn from(pk: simple::PressedKey) -> Self {
         PressedKey::Simple(pk)
     }
 }
 
-impl From<tap_hold::PressedKey> for PressedKey {
+impl<const L: layered::LayerIndex> From<tap_hold::PressedKey> for PressedKey<L> {
     fn from(pk: tap_hold::PressedKey) -> Self {
         PressedKey::TapHold(pk)
     }
 }
 
-impl key::PressedKey for PressedKey {
+impl<const L: layered::LayerIndex> key::PressedKey for PressedKey<L> {
     type Event = Event;
-    type Key = Key;
+    type Key = Key<L>;
 
-    fn key_code(&self, key_definition: &Key) -> Option<u8> {
+    fn key_code(&self, key_definition: &Key<L>) -> Option<u8> {
         match self {
+            PressedKey::LayerModifier(pk) => match key_definition {
+                Key::LayerModifier(key_def) => pk.key_code(key_def),
+                _ => None,
+            },
+
             PressedKey::Simple(pk) => match key_definition {
                 Key::Simple(key_def) => Some(pk.key_code(key_def)),
                 _ => None,
@@ -96,7 +113,7 @@ impl key::PressedKey for PressedKey {
 
     fn handle_event(
         &mut self,
-        key_definition: &Key,
+        key_definition: &Key<L>,
         event: key::Event<Self::Event>,
     ) -> impl IntoIterator<Item = key::Event<Self::Event>> {
         if let PressedKey::TapHold(tap_hold) = self {
