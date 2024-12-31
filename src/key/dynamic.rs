e80a347552efa984fc70f0f7f40889e6773cf3f3
Richard Goulter <richard.goulter@gmail.com>
2024-12-31 19:47:14 +0700

    rust: impl dynamic::Key for composite::Event with simple::Key

--- src/key/dynamic.rs
+++ src/key/dynamic.rs
@@ -4,7 +4,7 @@ use crate::{input, key};
 
 use key::{Key as _, PressedKey as _};
 
-use key::simple;
+use key::{composite, simple};
 
 use super::ScheduledEvent;
 
@@ -47,24 +47,27 @@ impl DynamicKey {
     }
 }
 
-impl<const N: usize> Key<simple::Event, N> for DynamicKey {
-    type Context = ();
-    type ContextEvent = ();
+impl<const N: usize> Key<composite::Event, N> for DynamicKey {
+    type Context = composite::Context<0, simple::Key>;
+    type ContextEvent = composite::Event;
 
     fn handle_event(
         &mut self,
         context: &Self::Context,
-        event: key::Event<simple::Event>,
-    ) -> heapless::Vec<key::ScheduledEvent<simple::Event>, N> {
-        let mut scheduled_events = heapless::Vec::new();
+        event: key::Event<composite::Event>,
+    ) -> heapless::Vec<key::ScheduledEvent<composite::Event>, N> {
+        let mut scheduled_events: heapless::Vec<key::ScheduledEvent<composite::Event>, N> =
+            heapless::Vec::new();
 
         if let Some(mut pressed_key) = self.pressed_key {
-            scheduled_events.extend(
-                pressed_key
-                    .handle_event(event)
-                    .into_iter()
-                    .map(|ev| ScheduledEvent::immediate(ev)),
-            );
+            if let Ok(event) = event.try_into() {
+                scheduled_events.extend(
+                    pressed_key
+                        .handle_event(event)
+                        .into_iter()
+                        .map(|ev| ScheduledEvent::immediate(ev).into()),
+                );
+            }
 
             if let key::Event::Input(input::Event::Release { keymap_index }) = event {
                 if keymap_index == pressed_key.keymap_index {
@@ -73,8 +76,9 @@ impl<const N: usize> Key<simple::Event, N> for DynamicKey {
             }
         } else {
             if let key::Event::Input(input::Event::Press { keymap_index }) = event {
-                let (pressed_key, new_events) = self.key.new_pressed_key(context, keymap_index);
-                scheduled_events.extend(new_events.into_iter());
+                let (pressed_key, new_events) =
+                    self.key.new_pressed_key(context.into(), keymap_index);
+                scheduled_events.extend(new_events.into_iter().map(|sch_ev| sch_ev.into()));
                 self.pressed_key = Some(pressed_key);
             }
         }
