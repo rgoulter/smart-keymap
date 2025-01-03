0b0eef87513805dc4e046025edbaff57f01a9b63
Richard Goulter <richard.goulter@gmail.com>
2025-01-03 09:48:34 +0700

    rust: build.rs use cargo:rerun-if-env-changed

--- build.rs
+++ build.rs
@@ -1,9 +1,9 @@
 use std::env;
 
 fn main() {
+    println!("cargo:rerun-if-env-changed=SMART_KEYMAP_CUSTOM_KEYMAP");
     println!("cargo::rustc-check-cfg=cfg(custom_keymap)");
     if env::var("SMART_KEYMAP_CUSTOM_KEYMAP").is_ok() {
         println!("cargo:rustc-cfg=custom_keymap");
-        println!("cargo:rerun-if-changed=NULL");
     }
 }
