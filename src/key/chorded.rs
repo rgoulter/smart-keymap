63b00766fc1304d225feb2632e5aa46d163d77ef
Richard Goulter <richard.goulter@gmail.com>
2025-01-06 12:25:21 +0700

    rust: add a blank key::chorded

---
+++ src/key/chorded.rs
@@ -0,0 +1,170 @@
+/// Chords (also known as "combos") are a smart keymap feature where
+/// simultaneously pressing multiple keys results in the behaviour of another
+/// key. e.g. pressing 'qw' keys together might send "Backspace"
+///
+/// In this module,
+/// - [Context] stores the [Chord]s of the keymap.
+///   - [Chord] is defined in terms of keymap indices.
+/// - [Key] describes a key which is part of a chord
+///   - This includes its 'pass-through key';
+///     the behaviour of the key when the chord
+///     didn't succeed.
+///     (e.g. 'q' or 'w' for the chord 'qw').
+/// - [PressedKeyState] manages chord resolution.
+///   - If a timeout event is received for the key:
+///     - if the PKS does not have a satisfied chord,
+///       the PKS resolves to "Timed out",
+///       and behaves as the pass-through key.
+///     - if the PKS has a satisfied chord,
+///       the PKS resolves to "Chorded key",
+///       - and the primary PKS
+///         (the PKS with lowest index in the chord)
+///         behaves as the chorded key.
+///   - When a key press is received for some keymap index:
+///     - if the pressed key does not belong to any of  the chords related to
+///        that pressed key,
+///       the PKS resolves to "Interrupted",
+///       and behaves as the pass-through key.
+///     - Otherwise,
+///        when a key press belonging to the chords related to the pressed key occurs:
+///       - If a chord is fully satisfied (and there are no overlapping chords),
+///         the PKS resolves to "Chorded key",
+///         - and the primary PKS
+///           (the PKS with lowest index in the chord)
+///           behaves as the chorded key.
+///       - If a chord is fully satisfied (and there are overlapping chords),
+///         then the pressed key state remains pending,
+///       - Otherwise, the PKS remains pending.
+///   - When a key release event is received for some keymap index:
+///     - If the PKS is resolved:
+///       - as chorded: then the chorded key is released (if the PKS was pressing it),
+///       - as pass-through key: then the pass through key is released.
+///     - If the PKS is pending,
+///       - then the pass-through key is 'tapped'.
+use serde::Deserialize;
+
+use crate::{input, key};
+
+const MAX_CHORDS: usize = 16;
+
+/// Chords are defined by an (unordered) set of indices into the keymap.
+#[derive(Deserialize, Debug, Clone, Copy, PartialEq)]
+pub enum ChordIndices {
+    /// A chord from two keys.
+    Chord2(u16, u16),
+}
+
+/// A chord.
+///
+/// Pressing all of the keys of the keymap indices
+///  acts-as if pressing the key.
+#[derive(Deserialize, Debug, Clone, Copy, PartialEq)]
+pub struct Chord {
+    /// The indices of the keys that make up the chord.
+    keymap_indices: ChordIndices,
+    /// The 'key' the chord resolves to.
+    key: u8,
+}
+
+/// Chord definitions.
+///
+/// ChN: number of chords
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct Context {
+    chords: [Chord; MAX_CHORDS],
+}
+
+impl key::Context for Context {
+    type Event = Event;
+
+    fn handle_event(&mut self, _event: Self::Event) {}
+}
+
+/// Chorded key (with a pass-through key).
+#[derive(Deserialize, Debug, Clone, Copy, PartialEq)]
+pub struct Key<K: key::Key>(K);
+
+impl<K: key::Key> Key<K> {
+    fn new_pressed_key(
+        &self,
+        _context: &Context,
+        keymap_index: u16,
+    ) -> (
+        input::PressedKey<Self, PressedKeyState>,
+        key::PressedKeyEvents<Event>,
+    ) {
+        // (
+        //     input::PressedKey {
+        //         keymap_index,
+        //         key: *self,
+        //         pressed_key_state: PressedKeyState::new(keymap_index),
+        //     },
+        //     key::PressedKeyEvents::no_events(),
+        // )
+        todo!()
+    }
+}
+
+/// Events for chorded keys.
+#[derive(Debug, Clone, Copy, Eq, PartialEq, Ord, PartialOrd)]
+pub struct Event {}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum ChordSatisfaction {
+    Unsatisfied,
+    Satisfied,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum ChordResolution {
+    ChordedKey,
+    Interrupted,
+    TimedOut,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum Status {
+    /// Waiting for more [Event]s
+    Pending(ChordSatisfaction),
+    /// Chord resolved from [Event]s
+    Resolved(ChordResolution),
+}
+
+/// State for pressed keys.
+#[derive(Debug, PartialEq)]
+pub struct PressedKeyState {
+    status: Status,
+    pressed_indices: heapless::Vec<u16, 8>,
+}
+
+/// Convenience type alias.
+pub type PressedKey<K> = input::PressedKey<Key<K>, PressedKeyState>;
+
+impl PressedKeyState {
+    /// Constructs a new [PressedKeyState].
+    fn new(keymap_index: u16) -> Self {
+        let mut pressed_indices = heapless::Vec::new();
+        pressed_indices.push(keymap_index).unwrap();
+        Self {
+            status: Status::Pending(ChordSatisfaction::Unsatisfied),
+            pressed_indices,
+        }
+    }
+}
+
+// impl<K: key::Key> PressedKeyState {
+//     type Event = Event;
+
+//     fn handle_event_for(
+//         &mut self,
+//         _keymap_index: u16,
+//         _key: &Key<K>,
+//         _event: key::Event<Self::Event>,
+//     ) -> impl IntoIterator<Item = key::Event<Self::Event>> {
+//         None
+//     }
+
+//     fn key_output(&self, _key: &Key<K>) -> Option<key::KeyOutput> {
+//         None
+//     }
+// }
