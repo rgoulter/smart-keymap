7957e4441423ceba0cdfeb60e0c77d1567d5cba0
Richard Goulter <richard.goulter@gmail.com>
2025-01-07 20:12:06 +0700

    ncl: add script to make keymap.json from keymap.ncl

---
+++ ncl/scripts/keymap-ncl-to-json.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# $ keymap-ncl-to-json.sh path/to/keymap-directory
+#
+# Generates a keymap.json file for the keymap.ncl
+#  in the given directory.
+
+set -ex
+
+SCRIPTS_DIR="$(dirname "$0")"
+REPOSITORY_DIR="${SCRIPTS_DIR}/../.."
+NCL_DIR="${REPOSITORY_DIR}/ncl"
+
+KEYMAP_DIR="${1}"
+
+DEST="${KEYMAP_DIR}/keymap.json"
+
+nickel export \
+  --format=json \
+  --import-path="${KEYMAP_DIR}" \
+  --import-path="${NCL_DIR}" \
+  "${NCL_DIR}/keymap-ncl-to-json.ncl" \
+  > "${DEST}"
