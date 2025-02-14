cf39c2a6b939c9cf581ef6d07d7bee9171f8d88a
Richard Goulter <richard.goulter@gmail.com>
2025-02-14 14:14:32 +0700

    tests: tap_hold, hold on press: rolling

--- tests/rust/tap_hold/hold_on_interrupt_press.rs
+++ tests/rust/tap_hold/hold_on_interrupt_press.rs
@@ -39,11 +39,17 @@ fn rolled_presses_resolves_hold() {
     let mut actual_reports = DistinctReports::new();
 
     // Act
-    // Roll the keys: press 0, press 1, ...
+    // Roll the keys: press 0, press 1, release 0, release 1
     keymap.handle_input(input::Event::Press { keymap_index: 0 });
     actual_reports.update(keymap.report_output().as_hid_boot_keyboard_report());
 
     keymap.handle_input(input::Event::Press { keymap_index: 1 });
+    actual_reports.update(keymap.report_output().as_hid_boot_keyboard_report());
+
+    keymap.handle_input(input::Event::Release { keymap_index: 0 });
+    actual_reports.update(keymap.report_output().as_hid_boot_keyboard_report());
+
+    keymap.handle_input(input::Event::Release { keymap_index: 1 });
     actual_reports.update(keymap.report_output().as_hid_boot_keyboard_report());
 
     while keymap.has_scheduled_events() {
@@ -56,6 +62,8 @@ fn rolled_presses_resolves_hold() {
         [0, 0, 0, 0, 0, 0, 0, 0],
         [0x01, 0, 0, 0, 0, 0, 0, 0],
         [0x01, 0, 0x05, 0, 0, 0, 0, 0],
+        [0, 0, 0x05, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
     ];
     assert_eq!(expected_reports, actual_reports.reports());
 }
