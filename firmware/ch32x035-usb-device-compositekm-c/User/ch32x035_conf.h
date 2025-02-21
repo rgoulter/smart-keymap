d1ad49de8a7413738cedc439b05a2639cfed0c49
Richard Goulter <richard.goulter@gmail.com>
2025-02-21 22:12:24 +0700

    firmware: ch32: remove unnecessary includes

--- firmware/ch32x035-usb-device-compositekm-c/User/ch32x035_conf.h
+++ firmware/ch32x035-usb-device-compositekm-c/User/ch32x035_conf.h
@@ -10,22 +10,15 @@
 #ifndef __CH32X035_CONF_H
 #define __CH32X035_CONF_H
 
-#include "ch32x035_adc.h"
-#include "ch32x035_awu.h"
 #include "ch32x035_dbgmcu.h"
-#include "ch32x035_dma.h"
 #include "ch32x035_exti.h"
 #include "ch32x035_flash.h"
 #include "ch32x035_gpio.h"
-#include "ch32x035_i2c.h"
 #include "ch32x035_it.h"
-#include "ch32x035_iwdg.h"
 #include "ch32x035_misc.h"
 #include "ch32x035_pwr.h"
 #include "ch32x035_rcc.h"
-#include "ch32x035_spi.h"
 #include "ch32x035_tim.h"
 #include "ch32x035_usart.h"
-#include "ch32x035_wwdg.h"
 
 #endif
