3b6c41e110790068774857b0caea3665d59d6cc6
Richard Goulter <richard.goulter@gmail.com>
2025-02-21 11:56:04 +0700

    firmware: ch32: make format

--- firmware/ch32x035-usb-device-compositekm-c/User/ch32x035_it.c
+++ firmware/ch32x035-usb-device-compositekm-c/User/ch32x035_it.c
@@ -1,14 +1,12 @@
-/********************************** (C) COPYRIGHT *******************************
- * File Name          : ch32x035_it.c
- * Author             : WCH
- * Version            : V1.0.0
- * Date               : 2023/12/26
- * Description        : Main Interrupt Service Routines.
-*********************************************************************************
-* Copyright (c) 2021 Nanjing Qinheng Microelectronics Co., Ltd.
-* Attention: This software (modified or not) and binary are used for 
-* microcontroller manufactured by Nanjing Qinheng Microelectronics.
-*******************************************************************************/
+/********************************** (C) COPYRIGHT
+ ******************************** File Name          : ch32x035_it.c Author :
+ *WCH Version            : V1.0.0 Date               : 2023/12/26 Description :
+ *Main Interrupt Service Routines.
+ *********************************************************************************
+ * Copyright (c) 2021 Nanjing Qinheng Microelectronics Co., Ltd.
+ * Attention: This software (modified or not) and binary are used for
+ * microcontroller manufactured by Nanjing Qinheng Microelectronics.
+ *******************************************************************************/
 #include "ch32x035_it.h"
 
 void NMI_Handler(void) __attribute__((interrupt("WCH-Interrupt-fast")));
@@ -21,10 +19,8 @@ void HardFault_Handler(void) __attribute__((interrupt("WCH-Interrupt-fast")));
  *
  * @return  none
  */
-void NMI_Handler(void)
-{
-    while (1)
-  {
+void NMI_Handler(void) {
+  while (1) {
   }
 }
 
@@ -35,11 +31,7 @@ void NMI_Handler(void)
  *
  * @return  none
  */
-void HardFault_Handler(void)
-{
-  while (1)
-  {
+void HardFault_Handler(void) {
+  while (1) {
   }
 }
-
-
--- firmware/ch32x035-usb-device-compositekm-c/User/main.c
+++ firmware/ch32x035-usb-device-compositekm-c/User/main.c
@@ -1,12 +1,11 @@
-/********************************** (C) COPYRIGHT *******************************
- * File Name          : main.c
- * Author             : WCH
+/********************************** (C) COPYRIGHT
+ ******************************** File Name          : main.c Author : WCH
  * Version            : V1.0.0
  * Date               : 2023/12/26
  * Description        : Main program body.
  *********************************************************************************
  * Copyright (c) 2021 Nanjing Qinheng Microelectronics Co., Ltd.
- * Attention: This software (modified or not) and binary are used for 
+ * Attention: This software (modified or not) and binary are used for
  * microcontroller manufactured by Nanjing Qinheng Microelectronics.
  *******************************************************************************/
 
@@ -16,16 +15,16 @@
  *
  * The matrix scan code is generated from ncl/matrix_scan.ncl, using whatever
  *  board.ncl file it was applied with.
- * e.g. by default, with the WeAct CH32X core board: cols: B0, B1, rows: B3, B11.
+ * e.g. by default, with the WeAct CH32X core board: cols: B0, B1, rows: B3,
+ * B11.
  */
 
-#include <ch32x035_usbfs_device.h>
 #include "debug.h"
 #include "usbd_composite_km.h"
+#include <ch32x035_usbfs_device.h>
 
-
-extern uint8_t  KB_Data_Pack[ 8 ];
-extern uint8_t  PREV_KB_Data_Pack[ 8 ];
+extern uint8_t KB_Data_Pack[8];
+extern uint8_t PREV_KB_Data_Pack[8];
 
 /*********************************************************************
  * @fn      main
@@ -34,49 +33,45 @@ extern uint8_t  PREV_KB_Data_Pack[ 8 ];
  *
  * @return  none
  */
-int main(void)
-{
+int main(void) {
 
-    NVIC_PriorityGroupConfig(NVIC_PriorityGroup_1);
-    SystemCoreClockUpdate();
-    Delay_Init();
-    USART_Printf_Init(115200);
-    printf("SystemClk:%d\r\n", SystemCoreClock);
-    printf("ChipID:%08x\r\n", DBGMCU_GetCHIPID() );
+  NVIC_PriorityGroupConfig(NVIC_PriorityGroup_1);
+  SystemCoreClockUpdate();
+  Delay_Init();
+  USART_Printf_Init(115200);
+  printf("SystemClk:%d\r\n", SystemCoreClock);
+  printf("ChipID:%08x\r\n", DBGMCU_GetCHIPID());
 
-    /* Initialize GPIO for keyboard scan */
-    KB_Scan_Init( );
-    KB_Sleep_Wakeup_Cfg( );
-    printf( "KB Scan Init OK!\r\n" );
+  /* Initialize GPIO for keyboard scan */
+  KB_Scan_Init();
+  KB_Sleep_Wakeup_Cfg();
+  printf("KB Scan Init OK!\r\n");
 
-    /* Initialize timer for Keyboard and mouse scan timing */
-    TIM3_Init( 47999, 0 );
-    printf( "TIM3 Init OK!\r\n" );
+  /* Initialize timer for Keyboard and mouse scan timing */
+  TIM3_Init(47999, 0);
+  printf("TIM3 Init OK!\r\n");
 
+  static uint8_t sending = 0;
 
-    static uint8_t sending = 0;
-
-    /* Usb Init */
-    USBFS_RCC_Init( );
-    USBFS_Device_Init( ENABLE , PWR_VDD_SupplyVoltage());
-    USB_Sleep_Wakeup_CFG( );
-    while(1)
-    {
-        if(USBFS_DevEnumStatus)
-        {
-            if (memcmp( KB_Data_Pack, PREV_KB_Data_Pack, sizeof( KB_Data_Pack ) ) != 0)
-            {
-                if (sending == 0) {
-                    int status = USBFS_Endp_DataUp( DEF_UEP1, KB_Data_Pack, sizeof( KB_Data_Pack ), DEF_UEP_CPY_LOAD );
-                    sending = 1;
-                } else if (USBFS_Endp_Busy[ DEF_UEP1 ] == 0) {
-                    memcpy( PREV_KB_Data_Pack, KB_Data_Pack, sizeof( KB_Data_Pack ) );
-                    sending = 0;
-                }
-            }
-
-            /* Handle keyboard lighting */
-            KB_LED_Handle( );
+  /* Usb Init */
+  USBFS_RCC_Init();
+  USBFS_Device_Init(ENABLE, PWR_VDD_SupplyVoltage());
+  USB_Sleep_Wakeup_CFG();
+  while (1) {
+    if (USBFS_DevEnumStatus) {
+      if (memcmp(KB_Data_Pack, PREV_KB_Data_Pack, sizeof(KB_Data_Pack)) != 0) {
+        if (sending == 0) {
+          int status = USBFS_Endp_DataUp(
+              DEF_UEP1, KB_Data_Pack, sizeof(KB_Data_Pack), DEF_UEP_CPY_LOAD);
+          sending = 1;
+        } else if (USBFS_Endp_Busy[DEF_UEP1] == 0) {
+          memcpy(PREV_KB_Data_Pack, KB_Data_Pack, sizeof(KB_Data_Pack));
+          sending = 0;
         }
+      }
+
+      /* Handle keyboard lighting */
+      KB_LED_Handle();
     }
+  }
 }
