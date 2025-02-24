f20493c5914d49dbe6c79cf59c20d80aea0f1cbb
Richard Goulter <richard.goulter@gmail.com>
2025-02-24 16:38:59 +0700

    rust: use Chorded newtype in keymap unit test

--- src/keymap.rs
+++ src/keymap.rs
@@ -746,9 +746,9 @@ mod tests {
 
         // Assemble
         type Ctx = Context;
-        type K = composite::Layered<composite::TapHold<keyboard::Key>>;
-        let keys: Keys1<K, Context, Event, PressedKey> = Keys1::new((composite::Layered(
-            composite::TapHold(keyboard::Key::new(0x04)),
+        type K = composite::Chorded<composite::Layered<composite::TapHold<keyboard::Key>>>;
+        let keys: Keys1<K, Context, Event, PressedKey> = Keys1::new((composite::Chorded(
+            composite::Layered(composite::TapHold(keyboard::Key::new(0x04))),
         ),));
         let context: Ctx = composite::DEFAULT_CONTEXT;
         let mut keymap = Keymap::new(keys, context);
