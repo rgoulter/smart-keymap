83ce5a1efad8c00e28e98eef9bc604661666fea4
Richard Goulter <richard.goulter@gmail.com>
2025-02-25 15:59:52 +0700

    rust: keymap: update context with the input event

--- src/keymap.rs
+++ src/keymap.rs
@@ -386,6 +386,8 @@ impl<
                     }
                 });
 
+                self.context.handle_event(ev.into());
+
                 match ev {
                     input::Event::Press { keymap_index } => {
                         let key = &mut self.key_definitions[keymap_index as usize];
