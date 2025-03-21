47d263e2fc3434b9fd673ea69fc463f536325866
Richard Goulter <richard.goulter@gmail.com>
2025-03-21 09:08:38 +0700

    rust: update key state for key::composite::chorded

--- src/key/composite/chorded.rs
+++ src/key/composite/chorded.rs
@@ -3,33 +3,27 @@ use core::fmt::Debug;
 #[cfg(feature = "std")]
 use serde::Deserialize;
 
-use crate::{input, key};
+use crate::key;
 
 use super::BaseKey;
 use super::TapHoldKey;
-use super::{Context, Event};
-use super::{Layered, LayeredKey, LayeredNestable, LayeredPressedKey, LayeredPressedKeyState};
+use super::{Context, Event, KeyState, PendingKeyState, PressedKeyResult};
+use super::{Layered, LayeredKey, LayeredNestable};
 
 /// Trait for types which can be nested in [ChordedKey] variants.
 pub trait ChordedNestable:
-    key::Key<Context = Context, Event = Event, PressedKey = LayeredPressedKey<TapHoldKey<BaseKey>>>
-    + Copy
+    key::Key<
+        Context = Context,
+        Event = Event,
+        KeyState = KeyState,
+        PendingKeyState = PendingKeyState,
+    > + Copy
     + PartialEq
 {
-    /// Construct a 'full representation' of the nestable key.
-    fn as_fat_key(self) -> LayeredKey<TapHoldKey<BaseKey>>;
 }
 
-impl<K: LayeredNestable> ChordedNestable for Layered<K> {
-    fn as_fat_key(self) -> LayeredKey<TapHoldKey<BaseKey>> {
-        self.as_fat_key()
-    }
-}
-impl<K: LayeredNestable> ChordedNestable for LayeredKey<K> {
-    fn as_fat_key(self) -> LayeredKey<TapHoldKey<BaseKey>> {
-        self.as_fat_key()
-    }
-}
+impl<K: LayeredNestable> ChordedNestable for Layered<K> {}
+impl<K: LayeredNestable> ChordedNestable for LayeredKey<K> {}
 
 /// An aggregate of [key::Key] types.
 #[derive(Debug, Clone, Copy, PartialEq)]
@@ -51,81 +45,210 @@ pub struct Chorded<K: ChordedNestable>(pub K);
 impl<K: ChordedNestable> key::Key for key::chorded::Key<K> {
     type Context = Context;
     type Event = Event;
-    type PressedKey = ChordedPressedKey<LayeredKey<TapHoldKey<BaseKey>>>;
+    type PendingKeyState = PendingKeyState;
+    type KeyState = KeyState;
 
     fn new_pressed_key(
         &self,
         context: Self::Context,
-        keymap_index: u16,
-    ) -> (Self::PressedKey, key::PressedKeyEvents<Self::Event>) {
-        let fat_key = (*self).map_key(|k| k.as_fat_key());
-        let (pk, pke) = fat_key.new_pressed_key(context, keymap_index);
-        (
-            pk.map_pressed_key(ChordedKey::Chorded, ChordedPressedKeyState::Chorded),
-            pke,
-        )
+        key_path: key::KeyPath,
+    ) -> (PressedKeyResult, key::PressedKeyEvents<Self::Event>) {
+        self.new_pressed_key(context, key_path)
+    }
+
+    fn handle_event(
+        &self,
+        pending_state: &mut Self::PendingKeyState,
+        context: Self::Context,
+        key_path: key::KeyPath,
+        event: key::Event<Self::Event>,
+    ) -> (Option<Self::KeyState>, key::PressedKeyEvents<Self::Event>) {
+        let keymap_index: u16 = key_path[0];
+        match pending_state {
+            PendingKeyState::Chorded(ch_pks) => {
+                if let Ok(ch_ev) = event.try_into_key_event(|e| e.try_into()) {
+                    let ch_state = ch_pks.handle_event(context.into(), keymap_index, ch_ev);
+                    if let Some(ch_state) = ch_state {
+                        let (i, nk, is_chord) = match ch_state {
+                            key::chorded::ChordResolution::Chord => (1, self.chord, true),
+                            key::chorded::ChordResolution::Passthrough => {
+                                (0, self.passthrough, false)
+                            }
+                        };
+                        let (pkr, mut pke) = nk.new_pressed_key(context, key_path);
+                        // PRESSED KEY PATH: add Chord (0 = passthrough, 1 = chord)
+                        let pkr = pkr.add_path_item(i);
+
+                        let ks = match pkr {
+                            // "Pending key resolves into pending key" to be implemented later.
+                            key::PressedKeyResult::Pending(_, _) => todo!(),
+                            key::PressedKeyResult::Resolved(ks) => ks,
+                        };
+
+                        let ch_r_ev = key::chorded::Event::ChordResolved(is_chord);
+                        let sch_ev = key::ScheduledEvent::immediate(key::Event::key_event(
+                            keymap_index,
+                            ch_r_ev.into(),
+                        ));
+                        pke.add_event(sch_ev);
+
+                        (Some(ks), pke)
+                    } else {
+                        (None, key::PressedKeyEvents::no_events())
+                    }
+                } else {
+                    (None, key::PressedKeyEvents::no_events())
+                }
+            }
+            _ => (None, key::PressedKeyEvents::no_events()),
+        }
+    }
+
+    fn lookup(
+        &self,
+        path: &[u16],
+    ) -> &dyn key::Key<
+        Context = Self::Context,
+        Event = Self::Event,
+        PendingKeyState = Self::PendingKeyState,
+        KeyState = Self::KeyState,
+    > {
+        match path {
+            [] => self,
+            // 0 = passthrough, 1 = chord
+            [0, path @ ..] => self.passthrough.lookup(path),
+            [1, path @ ..] => self.chord.lookup(path),
+            _ => panic!(),
+        }
     }
 }
 
 impl<K: ChordedNestable> key::Key for key::chorded::AuxiliaryKey<K> {
     type Context = Context;
     type Event = Event;
-    type PressedKey = ChordedPressedKey<LayeredKey<TapHoldKey<BaseKey>>>;
+    type PendingKeyState = PendingKeyState;
+    type KeyState = KeyState;
 
     fn new_pressed_key(
         &self,
         context: Self::Context,
-        keymap_index: u16,
-    ) -> (Self::PressedKey, key::PressedKeyEvents<Self::Event>) {
-        let fat_key = (*self).map_key(|k| k.as_fat_key());
-        let (pk, pke) = fat_key.new_pressed_key(context, keymap_index);
-        (
-            pk.map_pressed_key(ChordedKey::Auxiliary, ChordedPressedKeyState::Auxiliary),
-            pke,
-        )
+        key_path: key::KeyPath,
+    ) -> (PressedKeyResult, key::PressedKeyEvents<Self::Event>) {
+        self.new_pressed_key(context, key_path)
+    }
+
+    fn handle_event(
+        &self,
+        pending_state: &mut Self::PendingKeyState,
+        context: Self::Context,
+        key_path: key::KeyPath,
+        event: key::Event<Self::Event>,
+    ) -> (Option<Self::KeyState>, key::PressedKeyEvents<Self::Event>) {
+        let keymap_index = key_path[0];
+        match pending_state {
+            PendingKeyState::Chorded(ch_pks) => {
+                if let Ok(ch_ev) = event.try_into_key_event(|e| e.try_into()) {
+                    let ch_state = ch_pks.handle_event(context.into(), keymap_index, ch_ev);
+                    if let Some(key::chorded::ChordResolution::Passthrough) = ch_state {
+                        let nk = self.passthrough;
+                        let (pkr, mut pke) = nk.new_pressed_key(context, key_path);
+
+                        // n.b. no need to add to key path; chorded aux_key only nests the passthrough key.
+
+                        let ks = match pkr {
+                            // "Pending key resolves into pending key" to be implemented later.
+                            key::PressedKeyResult::Pending(_, _) => todo!(),
+                            key::PressedKeyResult::Resolved(ks) => ks,
+                        };
+
+                        let ch_r_ev = key::chorded::Event::ChordResolved(false);
+                        let sch_ev = key::ScheduledEvent::immediate(key::Event::key_event(
+                            keymap_index,
+                            ch_r_ev.into(),
+                        ));
+                        pke.add_event(sch_ev);
+
+                        (Some(ks), pke)
+                    } else if let Some(key::chorded::ChordResolution::Chord) = ch_state {
+                        let ch_r_ev = key::chorded::Event::ChordResolved(true);
+                        let pke = key::PressedKeyEvents::event(key::Event::key_event(
+                            keymap_index,
+                            ch_r_ev.into(),
+                        ));
+
+                        (Some(KeyState::NoOp), pke)
+                    } else {
+                        (None, key::PressedKeyEvents::no_events())
+                    }
+                } else {
+                    (None, key::PressedKeyEvents::no_events())
+                }
+            }
+            _ => (None, key::PressedKeyEvents::no_events()),
+        }
+    }
+
+    fn lookup(
+        &self,
+        path: &[u16],
+    ) -> &dyn key::Key<
+        Context = Self::Context,
+        Event = Self::Event,
+        PendingKeyState = Self::PendingKeyState,
+        KeyState = Self::KeyState,
+    > {
+        match path {
+            [] => self,
+            _ => self.passthrough.lookup(path),
+        }
     }
 }
 
 impl<K: ChordedNestable> key::Key for ChordedKey<K> {
     type Context = Context;
     type Event = Event;
-    type PressedKey = ChordedPressedKey<LayeredKey<TapHoldKey<BaseKey>>>;
+    type PendingKeyState = PendingKeyState;
+    type KeyState = KeyState;
 
     fn new_pressed_key(
         &self,
         context: Self::Context,
-        keymap_index: u16,
-    ) -> (Self::PressedKey, key::PressedKeyEvents<Self::Event>) {
+        key_path: key::KeyPath,
+    ) -> (PressedKeyResult, key::PressedKeyEvents<Self::Event>) {
         match self {
-            ChordedKey::Chorded(key) => key::Key::new_pressed_key(key, context, keymap_index),
-            ChordedKey::Auxiliary(chorded) => {
-                key::Key::new_pressed_key(chorded, context, keymap_index)
-            }
-            ChordedKey::Pass(key) => {
-                let (passthrough_pk, pke) = key.new_pressed_key(context, keymap_index);
-                let pk = input::PressedKey {
-                    key: ChordedKey::Pass(passthrough_pk.key),
-                    keymap_index,
-                    pressed_key_state:
-                        ChordedPressedKeyState::<LayeredKey<TapHoldKey<BaseKey>>>::Pass(
-                            passthrough_pk.pressed_key_state,
-                        ),
-                };
-                (pk, pke)
-            }
+            ChordedKey::Chorded(key) => key.new_pressed_key(context, key_path),
+            ChordedKey::Auxiliary(key) => key.new_pressed_key(context, key_path),
+            ChordedKey::Pass(key) => key.new_pressed_key(context, key_path),
         }
     }
-}
 
-impl<K: ChordedNestable> ChordedKey<K> {
-    /// Constructs a 'fat' key value from the given chorded key.
-    pub fn as_fat_key(self) -> ChordedKey<LayeredKey<TapHoldKey<BaseKey>>> {
+    fn handle_event(
+        &self,
+        pending_state: &mut Self::PendingKeyState,
+        context: Self::Context,
+        key_path: key::KeyPath,
+        event: key::Event<Self::Event>,
+    ) -> (Option<Self::KeyState>, key::PressedKeyEvents<Self::Event>) {
         match self {
-            ChordedKey::Chorded(key) => ChordedKey::Chorded(key.map_key(|k| k.as_fat_key())),
-            ChordedKey::Auxiliary(chorded) => {
-                ChordedKey::Auxiliary(chorded.map_key(|k| k.as_fat_key()))
-            }
-            ChordedKey::Pass(key) => ChordedKey::Pass(key.as_fat_key()),
+            ChordedKey::Chorded(key) => key.handle_event(pending_state, context, key_path, event),
+            ChordedKey::Auxiliary(key) => key.handle_event(pending_state, context, key_path, event),
+            ChordedKey::Pass(key) => key.handle_event(pending_state, context, key_path, event),
+        }
+    }
+
+    fn lookup(
+        &self,
+        path: &[u16],
+    ) -> &dyn key::Key<
+        Context = Self::Context,
+        Event = Self::Event,
+        PendingKeyState = Self::PendingKeyState,
+        KeyState = Self::KeyState,
+    > {
+        match self {
+            ChordedKey::Chorded(key) => key.lookup(path),
+            ChordedKey::Auxiliary(key) => key.lookup(path),
+            ChordedKey::Pass(key) => key.lookup(path),
         }
     }
 }
@@ -133,22 +256,40 @@ impl<K: ChordedNestable> ChordedKey<K> {
 impl<K: ChordedNestable> key::Key for Chorded<K> {
     type Context = Context;
     type Event = Event;
-    type PressedKey = ChordedPressedKey<LayeredKey<TapHoldKey<BaseKey>>>;
+    type PendingKeyState = PendingKeyState;
+    type KeyState = KeyState;
 
     fn new_pressed_key(
         &self,
         context: Self::Context,
-        keymap_index: u16,
-    ) -> (Self::PressedKey, key::PressedKeyEvents<Self::Event>) {
+        key_path: key::KeyPath,
+    ) -> (PressedKeyResult, key::PressedKeyEvents<Self::Event>) {
         let Chorded(key) = self;
-        let (pk, pke) = <K as key::Key>::new_pressed_key(key, context, keymap_index);
-        (
-            pk.map_pressed_key(
-                |k| ChordedKey::Pass(k.as_fat_key()),
-                ChordedPressedKeyState::Pass,
-            ),
-            pke,
-        )
+        key.new_pressed_key(context, key_path)
+    }
+
+    fn handle_event(
+        &self,
+        pending_state: &mut Self::PendingKeyState,
+        context: Self::Context,
+        key_path: key::KeyPath,
+        event: key::Event<Self::Event>,
+    ) -> (Option<Self::KeyState>, key::PressedKeyEvents<Self::Event>) {
+        let Chorded(key) = self;
+        key.handle_event(pending_state, context, key_path, event)
+    }
+
+    fn lookup(
+        &self,
+        path: &[u16],
+    ) -> &dyn key::Key<
+        Context = Self::Context,
+        Event = Self::Event,
+        PendingKeyState = Self::PendingKeyState,
+        KeyState = Self::KeyState,
+    > {
+        let Chorded(key) = self;
+        key.lookup(path)
     }
 }
 
@@ -173,62 +314,5 @@ impl<K: LayeredNestable> ChordedKey<LayeredKey<K>> {
     /// Constructs a [Key] from the given [key::layered::LayeredKey].
     pub const fn layered(key: key::layered::LayeredKey<K>) -> Self {
         ChordedKey::Pass(LayeredKey::Layered(key))
-    }
-}
-
-/// Aggregates the [key::PressedKeyState] types.
-#[derive(Debug, PartialEq)]
-pub enum ChordedPressedKeyState<K: ChordedNestable> {
-    /// A chorded key's pressed state.
-    Chorded(key::chorded::PressedKeyState<K>),
-    /// An auxiliary chorded key's pressed state.
-    Auxiliary(key::chorded::PressedKeyState<K>),
-    /// Passthrough state.
-    Pass(LayeredPressedKeyState<TapHoldKey<BaseKey>>),
-}
-
-/// Convenience type alias for a [key::PressedKey] with a layered key.
-pub type ChordedPressedKey<K> = input::PressedKey<ChordedKey<K>, ChordedPressedKeyState<K>>;
-
-impl<K: Copy + Into<ChordedKey<NK>>, NK: ChordedNestable> key::PressedKeyState<K>
-    for ChordedPressedKeyState<NK>
-{
-    type Context = Context;
-    type Event = Event;
-
-    fn handle_event_for(
-        &mut self,
-        context: Context,
-        keymap_index: u16,
-        key: &K,
-        event: key::Event<Event>,
-    ) -> key::PressedKeyEvents<Event> {
-        let k: ChordedKey<NK> = (*key).into();
-
-        match (k, self) {
-            (ChordedKey::Chorded(key), ChordedPressedKeyState::Chorded(pks)) => {
-                pks.handle_event_for(context, keymap_index, &key, event)
-            }
-            (ChordedKey::Auxiliary(chorded), ChordedPressedKeyState::Auxiliary(pks)) => {
-                pks.handle_event_for(context, keymap_index, &chorded, event)
-            }
-            (ChordedKey::Pass(key), ChordedPressedKeyState::Pass(pks)) => {
-                let k: LayeredKey<TapHoldKey<BaseKey>> = key.as_fat_key();
-                let events = pks.handle_event_for(context, keymap_index, &k, event);
-                events.into_events()
-            }
-            _ => key::PressedKeyEvents::no_events(),
-        }
-    }
-
-    fn key_output(&self, key: &K) -> key::KeyOutputState {
-        let k: ChordedKey<LayeredKey<TapHoldKey<BaseKey>>> = (*key).into().as_fat_key();
-
-        match (k, self) {
-            (ChordedKey::Chorded(_), ChordedPressedKeyState::Chorded(pks)) => pks.key_output(),
-            (ChordedKey::Auxiliary(_), ChordedPressedKeyState::Auxiliary(pks)) => pks.key_output(),
-            (ChordedKey::Pass(key), ChordedPressedKeyState::Pass(pks)) => pks.key_output(&key),
-            _ => key::KeyOutputState::no_output(),
-        }
     }
 }
