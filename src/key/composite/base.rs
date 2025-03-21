608374b42900ca8958eb6ad542a5fb1ca658f90d
Richard Goulter <richard.goulter@gmail.com>
2025-03-21 09:08:04 +0700

    rust: update key state for key::composite::base

--- src/key/composite/base.rs
+++ src/key/composite/base.rs
@@ -3,11 +3,11 @@ use core::fmt::Debug;
 #[cfg(feature = "std")]
 use serde::Deserialize;
 
-use crate::{input, key};
+use crate::key;
 use key::keyboard;
 use key::layered;
 
-use super::{Context, Event};
+use super::{Context, Event, KeyState, PendingKeyState, PressedKeyResult};
 
 /// An aggregate of [key::Key] types.
 #[derive(Debug, Clone, Copy, PartialEq)]
@@ -23,62 +23,124 @@ pub enum BaseKey {
 impl key::Key for layered::ModifierKey {
     type Context = Context;
     type Event = Event;
-    type PressedKey = BasePressedKey;
+    type PendingKeyState = PendingKeyState;
+    type KeyState = KeyState;
 
     fn new_pressed_key(
         &self,
         _context: Self::Context,
-        keymap_index: u16,
-    ) -> (Self::PressedKey, key::PressedKeyEvents<Self::Event>) {
-        let (pks, lmod_ev) = self.new_pressed_key();
-        let pk = BasePressedKey {
-            key: (*self).into(),
-            keymap_index,
-            pressed_key_state: pks.into(),
-        };
+        key_path: key::KeyPath,
+    ) -> (PressedKeyResult, key::PressedKeyEvents<Self::Event>) {
+        let keymap_index: u16 = key_path[0];
+        let (m_ks, lmod_ev) = self.new_pressed_key();
+        let pks = key::PressedKeyResult::Resolved(KeyState::LayerModifier(m_ks));
         let pke = key::PressedKeyEvents::event(key::Event::key_event(
             keymap_index,
             Event::LayerModification(lmod_ev),
         ));
-        (pk.into_pressed_key(), pke)
+        (pks, pke)
+    }
+
+    fn handle_event(
+        &self,
+        _pending_state: &mut Self::PendingKeyState,
+        _context: Self::Context,
+        _key_path: key::KeyPath,
+        _event: key::Event<Self::Event>,
+    ) -> (Option<Self::KeyState>, key::PressedKeyEvents<Self::Event>) {
+        panic!()
+    }
+
+    fn lookup(
+        &self,
+        _path: &[u16],
+    ) -> &dyn key::Key<
+        Context = Self::Context,
+        Event = Self::Event,
+        PendingKeyState = Self::PendingKeyState,
+        KeyState = Self::KeyState,
+    > {
+        self
     }
 }
 
 impl key::Key for keyboard::Key {
     type Context = Context;
     type Event = Event;
-    type PressedKey = BasePressedKey;
+    type PendingKeyState = PendingKeyState;
+    type KeyState = KeyState;
 
     fn new_pressed_key(
         &self,
         _context: Self::Context,
-        keymap_index: u16,
-    ) -> (Self::PressedKey, key::PressedKeyEvents<Self::Event>) {
-        let pks = self.new_pressed_key();
-        let pk = BasePressedKey {
-            key: (*self).into(),
-            keymap_index,
-            pressed_key_state: pks.into(),
-        };
+        _key_path: key::KeyPath,
+    ) -> (PressedKeyResult, key::PressedKeyEvents<Self::Event>) {
+        let k_ks = self.new_pressed_key();
+        let pks = key::PressedKeyResult::Resolved(KeyState::Keyboard(k_ks));
         let pke = key::PressedKeyEvents::no_events();
-        (pk.into_pressed_key(), pke)
+        (pks, pke)
+    }
+
+    fn handle_event(
+        &self,
+        _pending_state: &mut Self::PendingKeyState,
+        _context: Self::Context,
+        _key_path: key::KeyPath,
+        _event: key::Event<Self::Event>,
+    ) -> (Option<Self::KeyState>, key::PressedKeyEvents<Self::Event>) {
+        panic!()
+    }
+
+    fn lookup(
+        &self,
+        _path: &[u16],
+    ) -> &dyn key::Key<
+        Context = Self::Context,
+        Event = Self::Event,
+        PendingKeyState = Self::PendingKeyState,
+        KeyState = Self::KeyState,
+    > {
+        self
     }
 }
 
 impl key::Key for BaseKey {
     type Context = Context;
     type Event = Event;
-    type PressedKey = BasePressedKey;
+    type PendingKeyState = PendingKeyState;
+    type KeyState = KeyState;
 
     fn new_pressed_key(
         &self,
         context: Self::Context,
-        keymap_index: u16,
-    ) -> (Self::PressedKey, key::PressedKeyEvents<Self::Event>) {
+        key_path: key::KeyPath,
+    ) -> (PressedKeyResult, key::PressedKeyEvents<Self::Event>) {
         match self {
-            BaseKey::Keyboard(key) => key::Key::new_pressed_key(key, context, keymap_index),
-            BaseKey::LayerModifier(key) => key::Key::new_pressed_key(key, context, keymap_index),
+            BaseKey::Keyboard(key) => key::Key::new_pressed_key(key, context, key_path),
+            BaseKey::LayerModifier(key) => key::Key::new_pressed_key(key, context, key_path),
         }
+    }
+
+    fn handle_event(
+        &self,
+        _pending_state: &mut Self::PendingKeyState,
+        _context: Self::Context,
+        _key_path: key::KeyPath,
+        _event: key::Event<Self::Event>,
+    ) -> (Option<Self::KeyState>, key::PressedKeyEvents<Self::Event>) {
+        panic!()
+    }
+
+    fn lookup(
+        &self,
+        _path: &[u16],
+    ) -> &dyn key::Key<
+        Context = Self::Context,
+        Event = Self::Event,
+        PendingKeyState = Self::PendingKeyState,
+        KeyState = Self::KeyState,
+    > {
+        self
     }
 }
 
@@ -103,71 +165,5 @@ impl BaseKey {
     /// Constructs a [BaseKey::LayerModifier] from the given [layered::ModifierKey].
     pub const fn layer_modifier(key: layered::ModifierKey) -> Self {
         Self::LayerModifier(key)
-    }
-}
-
-/// Aggregates the [key::PressedKeyState] types.
-#[derive(Debug, PartialEq)]
-pub enum BasePressedKeyState {
-    /// A keyboard key's pressed state.
-    Keyboard(keyboard::PressedKeyState),
-    /// A layer modifier key's pressed state.
-    LayerModifier(layered::PressedModifierKeyState),
-}
-
-/// Convenience type alias for a [key::PressedKey] with a base key.
-pub type BasePressedKey = input::PressedKey<BaseKey, BasePressedKeyState>;
-
-impl<K: Copy + Into<BaseKey>> key::PressedKeyState<K> for BasePressedKeyState {
-    type Context = Context;
-    type Event = Event;
-
-    fn handle_event_for(
-        &mut self,
-        _context: Context,
-        keymap_index: u16,
-        key: &K,
-        event: key::Event<Event>,
-    ) -> key::PressedKeyEvents<Event> {
-        let bk: BaseKey = (*key).into();
-
-        match (bk, self) {
-            (BaseKey::LayerModifier(key), BasePressedKeyState::LayerModifier(pks)) => {
-                if let Ok(ev) = event.try_into_key_event(|e| e.try_into()) {
-                    let events = pks.handle_event_for(keymap_index, &key, ev);
-                    match events {
-                        Some(ev) => key::PressedKeyEvents::event(key::Event::key_event(
-                            keymap_index,
-                            Event::LayerModification(ev),
-                        )),
-                        None => key::PressedKeyEvents::no_events(),
-                    }
-                } else {
-                    key::PressedKeyEvents::no_events()
-                }
-            }
-            _ => key::PressedKeyEvents::no_events(),
-        }
-    }
-
-    fn key_output(&self, key: &K) -> key::KeyOutputState {
-        let bk: BaseKey = (*key).into();
-
-        match (bk, self) {
-            (BaseKey::Keyboard(key), BasePressedKeyState::Keyboard(pks)) => pks.key_output(&key),
-            _ => key::KeyOutputState::no_output(),
-        }
-    }
-}
-
-impl From<keyboard::PressedKeyState> for BasePressedKeyState {
-    fn from(pks: keyboard::PressedKeyState) -> Self {
-        BasePressedKeyState::Keyboard(pks)
-    }
-}
-
-impl From<layered::PressedModifierKeyState> for BasePressedKeyState {
-    fn from(pks: layered::PressedModifierKeyState) -> Self {
-        BasePressedKeyState::LayerModifier(pks)
     }
 }
