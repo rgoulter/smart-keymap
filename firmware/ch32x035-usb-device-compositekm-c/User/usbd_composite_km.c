0ef26446c6d98d343d7ae8590f256c4ce341c1ea
Richard Goulter <richard.goulter@gmail.com>
2025-03-21 21:22:20 +0700

    firmware: ch32x: rename header

--- firmware/ch32x035-usb-device-compositekm-c/User/usbd_composite_km.c
+++ firmware/ch32x035-usb-device-compositekm-c/User/usbd_composite_km.c
@@ -28,7 +28,7 @@
 #include "ch32x035_usbfs_device.h"
 #include "system_ch32x035.h"
 
-#include "keyboard_ch32x_48.h"
+#include "keyboard_ch32x.h"
 #include "smart_keymap.h"
 
 /*******************************************************************************/
