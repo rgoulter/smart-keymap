d442e2d08a3daad39906a89a16b33255e4250bfc
Richard Goulter <richard.goulter@gmail.com>
2025-03-27 11:29:56 +0700

    usbd-smart-keyboard: split transport use smart keymap event

--- usbd-smart-keyboard/src/split/transport.rs
+++ usbd-smart-keyboard/src/split/transport.rs
@@ -1,4 +1,7 @@
-use keyberon::layout::Event;
+use smart_keymap::input::Event;
+
+/// Length of the buffer used to serialize [LayoutMessage]s.
+pub const BUFFER_LENGTH: usize = 3;
 
 /// Messages for the RTIC task which manages the Keyberon layout.
 #[derive(Debug)]
@@ -16,8 +19,12 @@ pub enum LayoutMessage {
 #[allow(clippy::result_unit_err)]
 pub fn de(bytes: &[u8]) -> Result<Event, ()> {
     match *bytes {
-        [b'P', i, j, b'\n'] => Ok(Event::Press(i, j)),
-        [b'R', i, j, b'\n'] => Ok(Event::Release(i, j)),
+        [b'P', i, b'\n'] => Ok(Event::Press {
+            keymap_index: i as u16,
+        }),
+        [b'R', i, b'\n'] => Ok(Event::Release {
+            keymap_index: i as u16,
+        }),
         _ => Err(()),
     }
 }
@@ -26,19 +33,19 @@ pub fn de(bytes: &[u8]) -> Result<Event, ()> {
 ///
 /// The serialisation format must be compatible with
 /// the serialisation format in `de`.
-pub fn ser(e: Event) -> [u8; 4] {
+pub fn ser(e: Event) -> [u8; BUFFER_LENGTH] {
     match e {
-        Event::Press(i, j) => [b'P', i, j, b'\n'],
-        Event::Release(i, j) => [b'R', i, j, b'\n'],
+        Event::Press { keymap_index } => [b'P', keymap_index as u8, b'\n'],
+        Event::Release { keymap_index } => [b'R', keymap_index as u8, b'\n'],
     }
 }
 
 /// Deserialise an array of bytes into maybe a Keyberon Event.
-pub fn receive_byte(buf: &mut [u8; 4], b: u8) -> Option<Event> {
+pub fn receive_byte(buf: &mut [u8; BUFFER_LENGTH], b: u8) -> Option<Event> {
     buf.rotate_left(1);
-    buf[3] = b;
+    buf[BUFFER_LENGTH - 1] = b;
 
-    if buf[3] == b'\n' {
+    if buf[BUFFER_LENGTH - 1] == b'\n' {
         de(&buf[..]).ok()
     } else {
         None
