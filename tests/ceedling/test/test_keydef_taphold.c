353465a32c4376aad98e1c9a8c371593e6f35417
Richard Goulter <richard.goulter@gmail.com>
2025-03-29 21:53:20 +0700

    tests: ceedling: use keymap_register_input_event

--- tests/ceedling/test/test_keydef_taphold.c
+++ tests/ceedling/test/test_keydef_taphold.c
@@ -10,67 +10,73 @@
 #define MOD_LCTL 0x1
 #define MOD_LSFT 0x2
 
-void setUp(void) {
-}
+void setUp(void) {}
 
-void tearDown(void) {
-}
+void tearDown(void) {}
 
 void test_taphold_dth_uth_is_tap(void) {
-    // Pressing T.H., then releasing T.H., is same as tapping the tap key.
-    // (Check the tap key gets pressed).
+  // Pressing T.H., then releasing T.H., is same as tapping the tap key.
+  // (Check the tap key gets pressed).
 
-    uint8_t expected_report[8] = {0, 0, KC_C, 0, 0, 0, 0, 0};
-    uint8_t actual_report[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+  uint8_t expected_report[8] = {0, 0, KC_C, 0, 0, 0, 0, 0};
+  uint8_t actual_report[8] = {0, 0, 0, 0, 0, 0, 0, 0};
 
-    keymap_init();
+  keymap_init();
 
-    keymap_register_input_keypress(0); // First key in keymap is TapHold(C, _)
-    keymap_tick(actual_report);
-    keymap_register_input_keyrelease(0);
+  keymap_register_input_event((struct KeymapInputEvent){
+      .event_type = KeymapEventPress,
+      .value = 0}); // First key in keymap is TapHold(C, _)
+  keymap_tick(actual_report);
+  keymap_register_input_event(
+      (struct KeymapInputEvent){.event_type = KeymapEventRelease, .value = 0});
 
-    keymap_tick(actual_report);
-    TEST_ASSERT_EQUAL_UINT8_ARRAY(expected_report, actual_report, 8);
+  keymap_tick(actual_report);
+  TEST_ASSERT_EQUAL_UINT8_ARRAY(expected_report, actual_report, 8);
 }
 
 void test_taphold_dth_uth_eventually_clears(void) {
-    // Pressing T.H., then releasing T.H., is same as tapping the tap key.
-    // (Check the tap key releases).
+  // Pressing T.H., then releasing T.H., is same as tapping the tap key.
+  // (Check the tap key releases).
 
-    uint8_t expected_report[8] = {0, 0, 0, 0, 0, 0, 0, 0};
-    uint8_t actual_report[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+  uint8_t expected_report[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+  uint8_t actual_report[8] = {0, 0, 0, 0, 0, 0, 0, 0};
 
-    keymap_init();
+  keymap_init();
 
-    keymap_register_input_keypress(0); // First key in keymap is TapHold(C, _)
+  keymap_register_input_event((struct KeymapInputEvent){
+      .event_type = KeymapEventPress,
+      .value = 0}); // First key in keymap is TapHold(C, _)
+  keymap_tick(actual_report);
+  keymap_register_input_event(
+      (struct KeymapInputEvent){.event_type = KeymapEventRelease, .value = 0});
+
+  keymap_tick(actual_report);
+
+  // The 'tap' from the TapHold key should be released.
+  // after ... an implementation-specific number of ticks.
+  for (int i = 0; i < 50; i++) {
     keymap_tick(actual_report);
-    keymap_register_input_keyrelease(0);
+  }
 
-    keymap_tick(actual_report);
-
-    // The 'tap' from the TapHold key should be released.
-    // after ... an implementation-specific number of ticks.
-    for (int i = 0; i < 50; i++) {
-        keymap_tick(actual_report);
-    }
-
-    TEST_ASSERT_EQUAL_UINT8_ARRAY(expected_report, actual_report, 8);
+  TEST_ASSERT_EQUAL_UINT8_ARRAY(expected_report, actual_report, 8);
 }
 
 void test_taphold_dth_eventually_holds(void) {
-    // Pressing T.H., is eventually the same as holding the hold key.
+  // Pressing T.H., is eventually the same as holding the hold key.
 
-    uint8_t expected_report[8] = {MOD_LCTL, 0, 0, 0, 0, 0, 0, 0};
-    uint8_t actual_report[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+  uint8_t expected_report[8] = {MOD_LCTL, 0, 0, 0, 0, 0, 0, 0};
+  uint8_t actual_report[8] = {0, 0, 0, 0, 0, 0, 0, 0};
 
-    keymap_init();
+  keymap_init();
 
-    keymap_register_input_keypress(0); // First key in keymap is TapHold(C, _)
+  keymap_register_input_event((struct KeymapInputEvent){
+      .event_type = KeymapEventPress,
+      .value = 0}); // First key in keymap is TapHold(C, _)
 
-    // Wait 500ms
-    for (int i = 0; i < 500; i++) {
-        keymap_tick(actual_report);
-    }
+  // Wait 500ms
+  for (int i = 0; i < 500; i++) {
+    keymap_tick(actual_report);
+  }
 
-    TEST_ASSERT_EQUAL_UINT8_ARRAY(expected_report, actual_report, 8);
+  TEST_ASSERT_EQUAL_UINT8_ARRAY(expected_report, actual_report, 8);
 }
--- tests/ceedling/test/test_keymap.c
+++ tests/ceedling/test/test_keymap.c
@@ -5,115 +5,135 @@
 #define KC_A 0x04
 #define KC_B 0x05
 
-void setUp(void) {
-}
+void setUp(void) {}
 
-void tearDown(void) {
-}
+void tearDown(void) {}
 
 void test_copy_hid_boot_keyboard_report(void) {
-    uint8_t expected_report[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+  uint8_t expected_report[8] = {0, 0, 0, 0, 0, 0, 0, 0};
 
-    uint8_t actual_report[8] = {1, 2, 3, 4, 5, 6, 7, 8};
-    copy_hid_boot_keyboard_report(actual_report);
+  uint8_t actual_report[8] = {1, 2, 3, 4, 5, 6, 7, 8};
+  copy_hid_boot_keyboard_report(actual_report);
 
-    TEST_ASSERT_EQUAL_UINT8_ARRAY(expected_report, actual_report, 8);
+  TEST_ASSERT_EQUAL_UINT8_ARRAY(expected_report, actual_report, 8);
 }
 
 // KEYMAP: [A, A, A, B]
 
 void test_keyboard_keypress(void) {
-    uint8_t expected_report[8] = {0, 0, KC_A, 0, 0, 0, 0, 0};
-    uint8_t actual_report[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+  uint8_t expected_report[8] = {0, 0, KC_A, 0, 0, 0, 0, 0};
+  uint8_t actual_report[8] = {0, 0, 0, 0, 0, 0, 0, 0};
 
-    keymap_init();
+  keymap_init();
 
-    keymap_register_input_keypress(2); // Third key in the keymap is A
+  keymap_register_input_event(
+      (struct KeymapInputEvent){.event_type = KeymapEventPress,
+                                .value = 2}); // Third key in the keymap is A
 
-    copy_hid_boot_keyboard_report(actual_report);
+  copy_hid_boot_keyboard_report(actual_report);
 
-    TEST_ASSERT_EQUAL_UINT8_ARRAY(expected_report, actual_report, 8);
+  TEST_ASSERT_EQUAL_UINT8_ARRAY(expected_report, actual_report, 8);
 }
 
 void test_keyboard_keyrelease(void) {
-    uint8_t expected_report[8] = {0, 0, 0, 0, 0, 0, 0, 0};
-    uint8_t actual_report[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+  uint8_t expected_report[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+  uint8_t actual_report[8] = {0, 0, 0, 0, 0, 0, 0, 0};
 
-    keymap_init();
+  keymap_init();
 
-    keymap_register_input_keypress(2);
-    keymap_tick(actual_report);
-    keymap_register_input_keyrelease(2);
+  keymap_register_input_event(
+      (struct KeymapInputEvent){.event_type = KeymapEventPress, .value = 2});
+  keymap_tick(actual_report);
+  keymap_register_input_event(
+      (struct KeymapInputEvent){.event_type = KeymapEventRelease, .value = 2});
 
-    copy_hid_boot_keyboard_report(actual_report);
+  copy_hid_boot_keyboard_report(actual_report);
 
-    TEST_ASSERT_EQUAL_UINT8_ARRAY(expected_report, actual_report, 8);
+  TEST_ASSERT_EQUAL_UINT8_ARRAY(expected_report, actual_report, 8);
 }
 
 void test_keyboard_keypress_sequence_da_db(void) {
-    // Pressing A, then B, should report "A B"
+  // Pressing A, then B, should report "A B"
 
-    uint8_t expected_report[8] = {0, 0, KC_A, KC_B, 0, 0, 0, 0};
-    uint8_t actual_report[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+  uint8_t expected_report[8] = {0, 0, KC_A, KC_B, 0, 0, 0, 0};
+  uint8_t actual_report[8] = {0, 0, 0, 0, 0, 0, 0, 0};
 
-    keymap_init();
+  keymap_init();
 
-    keymap_register_input_keypress(2); // Third key in the keymap is A
-    keymap_tick(actual_report);
-    keymap_register_input_keypress(3); // Fourth key in the keymap is B
+  keymap_register_input_event(
+      (struct KeymapInputEvent){.event_type = KeymapEventPress,
+                                .value = 2}); // Third key in the keymap is A
+  keymap_tick(actual_report);
+  keymap_register_input_event(
+      (struct KeymapInputEvent){.event_type = KeymapEventPress,
+                                .value = 3}); // Fourth key in the keymap is B
 
-    copy_hid_boot_keyboard_report(actual_report);
-    TEST_ASSERT_EQUAL_UINT8_ARRAY(expected_report, actual_report, 8);
+  copy_hid_boot_keyboard_report(actual_report);
+  TEST_ASSERT_EQUAL_UINT8_ARRAY(expected_report, actual_report, 8);
 }
 
 void test_keyboard_keypress_sequence_db_da(void) {
-    // Pressing B, then A, should report "B A"
+  // Pressing B, then A, should report "B A"
 
-    uint8_t expected_report[8] = {0, 0, KC_B, KC_A, 0, 0, 0, 0};
-    uint8_t actual_report[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+  uint8_t expected_report[8] = {0, 0, KC_B, KC_A, 0, 0, 0, 0};
+  uint8_t actual_report[8] = {0, 0, 0, 0, 0, 0, 0, 0};
 
-    keymap_init();
+  keymap_init();
 
-    keymap_register_input_keypress(3); // Fourth key in the keymap is B
-    keymap_tick(actual_report);
-    keymap_register_input_keypress(2); // Third key in the keymap is A
+  keymap_register_input_event(
+      (struct KeymapInputEvent){.event_type = KeymapEventPress,
+                                .value = 3}); // Fourth key in the keymap is B
+  keymap_tick(actual_report);
+  keymap_register_input_event(
+      (struct KeymapInputEvent){.event_type = KeymapEventPress,
+                                .value = 2}); // Third key in the keymap is A
 
-    copy_hid_boot_keyboard_report(actual_report);
-    TEST_ASSERT_EQUAL_UINT8_ARRAY(expected_report, actual_report, 8);
+  copy_hid_boot_keyboard_report(actual_report);
+  TEST_ASSERT_EQUAL_UINT8_ARRAY(expected_report, actual_report, 8);
 }
 
 void test_keyboard_keypress_sequence_da_db_ub(void) {
-    // Pressing A, then B; then releasing B, should report "A"
+  // Pressing A, then B; then releasing B, should report "A"
 
-    uint8_t expected_report[8] = {0, 0, KC_A, 0, 0, 0, 0, 0};
-    uint8_t actual_report[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+  uint8_t expected_report[8] = {0, 0, KC_A, 0, 0, 0, 0, 0};
+  uint8_t actual_report[8] = {0, 0, 0, 0, 0, 0, 0, 0};
 
-    keymap_init();
+  keymap_init();
 
-    keymap_register_input_keypress(2); // Third key in the keymap is A
-    keymap_tick(actual_report);
-    keymap_register_input_keypress(3); // Fourth key in the keymap is B
-    keymap_tick(actual_report);
-    keymap_register_input_keyrelease(3);
+  keymap_register_input_event(
+      (struct KeymapInputEvent){.event_type = KeymapEventPress,
+                                .value = 2}); // Third key in the keymap is A
+  keymap_tick(actual_report);
+  keymap_register_input_event(
+      (struct KeymapInputEvent){.event_type = KeymapEventPress,
+                                .value = 3}); // Fourth key in the keymap is B
+  keymap_tick(actual_report);
+  keymap_register_input_event(
+      (struct KeymapInputEvent){.event_type = KeymapEventRelease, .value = 3});
 
-    copy_hid_boot_keyboard_report(actual_report);
-    TEST_ASSERT_EQUAL_UINT8_ARRAY(expected_report, actual_report, 8);
+  copy_hid_boot_keyboard_report(actual_report);
+  TEST_ASSERT_EQUAL_UINT8_ARRAY(expected_report, actual_report, 8);
 }
 
 void test_keyboard_keypress_sequence_da_db_ua(void) {
-    // Pressing A, then B; then releasing A, should report "B"
+  // Pressing A, then B; then releasing A, should report "B"
 
-    uint8_t expected_report[8] = {0, 0, KC_B, 0, 0, 0, 0, 0};
-    uint8_t actual_report[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+  uint8_t expected_report[8] = {0, 0, KC_B, 0, 0, 0, 0, 0};
+  uint8_t actual_report[8] = {0, 0, 0, 0, 0, 0, 0, 0};
 
-    keymap_init();
+  keymap_init();
 
-    keymap_register_input_keypress(2); // Third key in the keymap is A
-    keymap_tick(actual_report);
-    keymap_register_input_keypress(3); // Fourth key in the keymap is B
-    keymap_tick(actual_report);
-    keymap_register_input_keyrelease(2);
+  keymap_register_input_event(
+      (struct KeymapInputEvent){.event_type = KeymapEventPress,
+                                .value = 2}); // Third key in the keymap is A
+  keymap_tick(actual_report);
+  keymap_register_input_event(
+      (struct KeymapInputEvent){.event_type = KeymapEventPress,
+                                .value = 3}); // Fourth key in the keymap is B
+  keymap_tick(actual_report);
+  keymap_register_input_event(
+      (struct KeymapInputEvent){.event_type = KeymapEventRelease, .value = 2});
 
-    copy_hid_boot_keyboard_report(actual_report);
-    TEST_ASSERT_EQUAL_UINT8_ARRAY(expected_report, actual_report, 8);
+  copy_hid_boot_keyboard_report(actual_report);
+  TEST_ASSERT_EQUAL_UINT8_ARRAY(expected_report, actual_report, 8);
 }
