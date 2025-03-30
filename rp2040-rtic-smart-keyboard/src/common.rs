b88d628dd678ca31e3804a067d4b7681c84c7ade
Richard Goulter <richard.goulter@gmail.com>
2025-03-30 19:59:48 +0700

    rp2040-rtic: add reset_to_bootloader fn

--- rp2040-rtic-smart-keyboard/src/common.rs
+++ rp2040-rtic-smart-keyboard/src/common.rs
@@ -11,3 +11,8 @@ pub type UsbDevice = usb_device::device::UsbDevice<'static, UsbBus>;
 
 /// Alias for [usbd_serial::SerialPort] using the RP2040 [UsbBus].
 pub type UsbSerial = usbd_serial::SerialPort<'static, UsbBus>;
+
+/// Enters the bootloader.
+pub fn keyboard_reset_to_bootloader() {
+    rp2040_hal::rom_data::reset_to_usb_boot(0, 0);
+}
