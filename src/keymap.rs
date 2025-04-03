b8090a9be0ab336fbc181aaad72e7ef6a3ea6ba3
Richard Goulter <richard.goulter@gmail.com>
2025-04-03 09:09:57 +0700

    rust: impl PartialEq for keymap::DistinctReports

--- src/keymap.rs
+++ src/keymap.rs
@@ -251,13 +251,50 @@ impl HIDKeyboardReporter {
 
 /// For tracking distinct HID reports from the keymap.
 #[cfg(feature = "std")]
-#[derive(Debug, Clone, Eq, PartialEq)]
+#[derive(Debug, Clone, Eq)]
 pub struct DistinctReports(Vec<[u8; 8]>);
 
 #[cfg(feature = "std")]
 impl Default for DistinctReports {
     fn default() -> Self {
         Self::new()
+    }
+}
+
+#[cfg(feature = "std")]
+impl core::cmp::PartialEq for DistinctReports {
+    fn eq(&self, other: &Self) -> bool {
+        // First element in DistinctReports should be [0; 8].
+        if self.0[0] != other.0[0] {
+            return false;
+        }
+
+        let mut i: usize = 1;
+        let mut j: usize = 1;
+
+        let self_len = self.0.len();
+        let other_len = other.0.len();
+
+        // Compare the rest of the elements.
+        while i < self_len && j < other_len {
+            // Ignore [0; 8] elements.
+            // (The reports are distinct; so, no two elements should be equal)
+            while (i < self_len - 1) && self.0[i] == [0; 8] {
+                i += 1;
+            }
+            while (j < other_len - 1) && other.0[j] == [0; 8] {
+                j += 1;
+            }
+
+            if self.0[i] != other.0[j] {
+                return false;
+            }
+
+            i += 1;
+            j += 1;
+        }
+
+        return i == self_len && j == other_len;
     }
 }
 
