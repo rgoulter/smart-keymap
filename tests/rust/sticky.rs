24889dabf299eda13c44b914d179750455cc29c2
Richard Goulter <richard.goulter@gmail.com>
2025-04-08 17:07:51 +0700

    tests: rust: test multiple sticky keys

--- tests/rust/sticky.rs
+++ tests/rust/sticky.rs
@@ -16,7 +16,7 @@ type KS = composite::KeyState;
 type SK = composite::Chorded<composite::Layered<composite::TapHold<sticky::Key>>>;
 type K = composite::Chorded<composite::Layered<composite::TapHold<keyboard::Key>>>;
 
-const KEYS: Keys4<SK, K, K, K, Ctx, Ev, PKS, KS> = tuples::Keys4::new((
+const KEYS: Keys4<SK, K, K, SK, Ctx, Ev, PKS, KS> = tuples::Keys4::new((
     composite::Chorded(composite::Layered(composite::TapHold(sticky::Key::new(
         key::KeyboardModifiers::LEFT_SHIFT,
     )))),
@@ -26,9 +26,9 @@ const KEYS: Keys4<SK, K, K, K, Ctx, Ev, PKS, KS> = tuples::Keys4::new((
     composite::Chorded(
         composite::Layered(composite::TapHold(keyboard::Key::new(0x05))), // B
     ),
-    composite::Chorded(
-        composite::Layered(composite::TapHold(keyboard::Key::new(0x2C))), // Space
-    ),
+    composite::Chorded(composite::Layered(composite::TapHold(sticky::Key::new(
+        key::KeyboardModifiers::LEFT_CTRL,
+    )))),
 ));
 
 const CONTEXT: Ctx = composite::DEFAULT_CONTEXT;
@@ -136,6 +136,45 @@ fn tap_sticky_mod_acts_as_regular_mod_when_interrupted_by_key() {
         [0, 0, 0, 0, 0, 0, 0, 0],
         [0x02, 0, 0x04, 0, 0, 0, 0, 0],
         [0, 0, 0x04, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+    ];
+    assert_eq!(expected_reports, actual_reports.reports());
+}
+
+#[test]
+fn tap_multiple_sticky_mod_modifies_next_keyboard_key() {
+    // Assemble
+    let mut keymap = Keymap::new(KEYS, CONTEXT);
+    let mut actual_reports = DistinctReports::new();
+
+    // Act
+    // Tap Sticky Modifiers
+    keymap.handle_input(input::Event::Press { keymap_index: 0 });
+    actual_reports.update(keymap.report_output().as_hid_boot_keyboard_report());
+    keymap.handle_input(input::Event::Release { keymap_index: 0 });
+    actual_reports.update(keymap.report_output().as_hid_boot_keyboard_report());
+    keymap.handle_input(input::Event::Press { keymap_index: 3 });
+    actual_reports.update(keymap.report_output().as_hid_boot_keyboard_report());
+    keymap.handle_input(input::Event::Release { keymap_index: 3 });
+    actual_reports.update(keymap.report_output().as_hid_boot_keyboard_report());
+
+    // Tap "A"
+    keymap.handle_input(input::Event::Press { keymap_index: 1 });
+    actual_reports.update(keymap.report_output().as_hid_boot_keyboard_report());
+    keymap.handle_input(input::Event::Release { keymap_index: 1 });
+    actual_reports.update(keymap.report_output().as_hid_boot_keyboard_report());
+
+    while keymap.has_scheduled_events() {
+        keymap.tick();
+        actual_reports.update(keymap.report_output().as_hid_boot_keyboard_report());
+    }
+
+    // Assert
+    let expected_reports: &[[u8; 8]] = &[
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0x02, 0, 0, 0, 0, 0, 0, 0],
+        [0x03, 0, 0, 0, 0, 0, 0, 0],
+        [0x03, 0, 0x04, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0],
     ];
     assert_eq!(expected_reports, actual_reports.reports());
