d16c7560f52a2031735cfb2441dafd652c98030a
Richard Goulter <richard.goulter@gmail.com>
2025-04-10 15:48:36 +0700

    rust: move key::Key impl to key::sticky

--- src/key/sticky.rs
+++ src/key/sticky.rs
@@ -208,6 +208,49 @@ impl Key {
     }
 }
 
+impl key::Key for Key {
+    type Context = crate::init::Context;
+    type Event = crate::init::Event;
+    type PendingKeyState = crate::init::PendingKeyState;
+    type KeyState = crate::init::KeyState;
+
+    fn new_pressed_key(
+        &self,
+        _context: Self::Context,
+        _key_path: key::KeyPath,
+    ) -> (
+        key::PressedKeyResult<Self::PendingKeyState, Self::KeyState>,
+        key::KeyEvents<Self::Event>,
+    ) {
+        let ks = self.new_pressed_key();
+        let pks = key::PressedKeyResult::Resolved(ks.into());
+        let pke = key::KeyEvents::no_events();
+        (pks, pke)
+    }
+
+    fn handle_event(
+        &self,
+        _pending_state: &mut Self::PendingKeyState,
+        _context: Self::Context,
+        _key_path: key::KeyPath,
+        _event: key::Event<Self::Event>,
+    ) -> (Option<Self::KeyState>, key::KeyEvents<Self::Event>) {
+        panic!()
+    }
+
+    fn lookup(
+        &self,
+        _path: &[u16],
+    ) -> &dyn key::Key<
+        Context = Self::Context,
+        Event = Self::Event,
+        PendingKeyState = Self::PendingKeyState,
+        KeyState = Self::KeyState,
+    > {
+        self
+    }
+}
+
 /// Whether the pressed Sticky modifier key is "sticky" or "regular".
 #[derive(Debug, Clone, Copy, Eq, PartialEq)]
 pub enum Behavior {
