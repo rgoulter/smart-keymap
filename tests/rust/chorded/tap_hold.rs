c623a416734b7d1a4e112e38d895fe8db1b12f1b
Richard Goulter <richard.goulter@gmail.com>
2025-04-10 21:36:59 +0700

    tests: rust: add tests for chord with tap-hold key

---
+++ tests/rust/chorded/tap_hold.rs
@@ -0,0 +1,99 @@
+use smart_keymap::input;
+use smart_keymap::key;
+use smart_keymap::keymap;
+use smart_keymap::tuples;
+
+use keymap::DistinctReports;
+use keymap::Keymap;
+
+use key::{chorded, composite, keyboard, tap_hold};
+use tuples::Keys2;
+
+type Ctx = composite::Context;
+type Ev = composite::Event;
+type PKS = composite::PendingKeyState;
+type KS = composite::KeyState;
+type CK = composite::ChordedKey<composite::Layered<composite::TapHoldKey<keyboard::Key>>>;
+type AK = composite::ChordedKey<composite::Layered<composite::TapHold<keyboard::Key>>>;
+
+const KEYS: Keys2<CK, AK, Ctx, Ev, PKS, KS> = tuples::Keys2::new((
+    composite::ChordedKey::Chorded(chorded::Key::new(
+        composite::Layered(composite::TapHoldKey::TapHold(tap_hold::Key::new(
+            keyboard::Key::new(0x06),
+            keyboard::Key::new(0xE0),
+        ))),
+        composite::Layered(composite::TapHoldKey::Pass(keyboard::Key::new(0x04))),
+    )),
+    composite::ChordedKey::Auxiliary(chorded::AuxiliaryKey::new(composite::Layered(
+        composite::TapHold(keyboard::Key::new(0x05)),
+    ))),
+));
+
+const CONTEXT: Ctx = key::composite::Context::from_config(composite::Config {
+    chorded: chorded::Config {
+        chords: [Some(chorded::ChordIndices::Chord2(0, 1)), None, None, None],
+        ..chorded::DEFAULT_CONFIG
+    },
+    ..composite::DEFAULT_CONFIG
+});
+
+#[test]
+fn tap_chord_acts_as_chorded_tap() {
+    // Assemble
+    let mut keymap = Keymap::new(KEYS, CONTEXT);
+    let mut actual_reports = DistinctReports::new();
+
+    // Act
+    keymap.handle_input(input::Event::Press { keymap_index: 0 });
+    actual_reports.update(keymap.report_output().as_hid_boot_keyboard_report());
+
+    keymap.handle_input(input::Event::Press { keymap_index: 1 });
+    actual_reports.update(keymap.report_output().as_hid_boot_keyboard_report());
+
+    keymap.handle_input(input::Event::Release { keymap_index: 0 });
+    actual_reports.update(keymap.report_output().as_hid_boot_keyboard_report());
+
+    keymap.handle_input(input::Event::Release { keymap_index: 1 });
+    actual_reports.update(keymap.report_output().as_hid_boot_keyboard_report());
+
+    while keymap.has_scheduled_events() {
+        keymap.tick();
+        actual_reports.update(keymap.report_output().as_hid_boot_keyboard_report());
+    }
+
+    // Assert
+    #[rustfmt::skip]
+    let expected_reports: &[[u8; 8]] = &[
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0x06, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+    ];
+    assert_eq!(expected_reports, actual_reports.reports());
+}
+
+#[test]
+fn hold_chord_acts_as_chorded_hold() {
+    // Assemble
+    let mut keymap = Keymap::new(KEYS, CONTEXT);
+    let mut actual_reports = DistinctReports::new();
+
+    // Act
+    keymap.handle_input(input::Event::Press { keymap_index: 0 });
+    actual_reports.update(keymap.report_output().as_hid_boot_keyboard_report());
+
+    keymap.handle_input(input::Event::Press { keymap_index: 1 });
+    actual_reports.update(keymap.report_output().as_hid_boot_keyboard_report());
+
+    while keymap.has_scheduled_events() {
+        keymap.tick();
+        actual_reports.update(keymap.report_output().as_hid_boot_keyboard_report());
+    }
+
+    // Assert
+    #[rustfmt::skip]
+    let expected_reports: &[[u8; 8]] = &[
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0, 0],
+    ];
+    assert_eq!(expected_reports, actual_reports.reports());
+}
