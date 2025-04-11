c246e5eb28a6f64e047bcd6dab04ca47b5311564
Richard Goulter <richard.goulter@gmail.com>
2025-04-11 19:25:03 +0700

    rust: move keymap::EventScheduler to its own module

--- src/keymap.rs
+++ src/keymap.rs
@@ -1,3 +1,5 @@
+mod event_scheduler;
+
 use core::cmp::PartialEq;
 use core::fmt::Debug;
 use core::marker::Copy;
@@ -10,8 +12,7 @@ use crate::key;
 
 use key::Event;
 
-const MAX_PENDING_EVENTS: usize = 32;
-const MAX_SCHEDULED_EVENTS: usize = 32;
+use event_scheduler::EventScheduler;
 
 /// Maximum number of pressed keys supported.
 pub const MAX_PRESSED_KEYS: usize = 16;
@@ -20,93 +21,6 @@ const MAX_QUEUED_INPUT_EVENTS: usize = 32;
 
 /// Number of ticks before the next input event is processed in tick().
 pub const INPUT_QUEUE_TICK_DELAY: u8 = 1;
-
-#[derive(Debug, Clone, Copy, Eq, PartialEq)]
-struct ScheduledEvent<E: Debug> {
-    time: u32,
-    event: Event<E>,
-}
-
-#[derive(Debug)]
-struct EventScheduler<E: Debug> {
-    pending_events: heapless::spsc::Queue<Event<E>, { MAX_PENDING_EVENTS }>,
-    scheduled_events: heapless::Vec<ScheduledEvent<E>, { MAX_SCHEDULED_EVENTS }>,
-    schedule_counter: u32,
-}
-
-impl<E: Debug> EventScheduler<E> {
-    pub const fn new() -> Self {
-        Self {
-            pending_events: heapless::spsc::Queue::new(),
-            scheduled_events: heapless::Vec::new(),
-            schedule_counter: 0,
-        }
-    }
-
-    pub fn init(&mut self) {
-        while self.pending_events.dequeue().is_some() {}
-        self.scheduled_events.clear();
-        self.schedule_counter = 0;
-    }
-
-    pub fn schedule_event(&mut self, scheduled_event: key::ScheduledEvent<E>) {
-        match scheduled_event.schedule {
-            key::Schedule::Immediate => {
-                self.enqueue_event(scheduled_event.event);
-            }
-            key::Schedule::After(delay) => {
-                self.schedule_after(delay as u32, scheduled_event.event);
-            }
-        }
-    }
-
-    pub fn enqueue_event(&mut self, event: Event<E>) {
-        self.pending_events.enqueue(event).unwrap();
-    }
-
-    pub fn schedule_after(&mut self, delay: u32, event: Event<E>) {
-        let time = self.schedule_counter + delay;
-        // binary sort insertion;
-        //  smallest at *end* (quick to pop off),
-        //  highest at *start*.
-        let pos = self
-            .scheduled_events
-            .binary_search_by(|sch_item| sch_item.time.cmp(&delay).reverse())
-            .unwrap_or_else(|e| e);
-        self.scheduled_events
-            .insert(pos, ScheduledEvent { time, event })
-            .unwrap();
-    }
-
-    pub fn cancel_events_for_keymap_index(&mut self, keymap_index: u16) {
-        self.scheduled_events
-            .retain(|ScheduledEvent { event, .. }| match event {
-                &Event::Key {
-                    keymap_index: ki, ..
-                } => ki != keymap_index,
-                _ => true,
-            });
-    }
-
-    pub fn tick(&mut self) {
-        self.schedule_counter += 1;
-        let scheduled_ready =
-            if let Some(&ScheduledEvent { time, .. }) = self.scheduled_events.last() {
-                time <= self.schedule_counter
-            } else {
-                false
-            };
-        if scheduled_ready {
-            if let Some(ScheduledEvent { event, .. }) = self.scheduled_events.pop() {
-                self.pending_events.enqueue(event).unwrap();
-            }
-        }
-    }
-
-    pub fn dequeue(&mut self) -> Option<Event<E>> {
-        self.pending_events.dequeue()
-    }
-}
 
 /// Constructs an HID report or a sequence of key codes from the given sequence of [key::KeyOutput].
 #[derive(Debug)]
---
+++ src/keymap/event_scheduler.rs
@@ -0,0 +1,97 @@
+use core::cmp::PartialEq;
+use core::fmt::Debug;
+use core::marker::Copy;
+
+use crate::key;
+
+use key::Event;
+
+pub(crate) const MAX_PENDING_EVENTS: usize = 32;
+pub(crate) const MAX_SCHEDULED_EVENTS: usize = 32;
+
+#[derive(Debug, Clone, Copy, Eq, PartialEq)]
+pub(crate) struct ScheduledEvent<E: Debug> {
+    time: u32,
+    event: Event<E>,
+}
+
+#[derive(Debug)]
+pub(crate) struct EventScheduler<E: Debug> {
+    pub(crate) pending_events: heapless::spsc::Queue<Event<E>, { MAX_PENDING_EVENTS }>,
+    pub(crate) scheduled_events: heapless::Vec<ScheduledEvent<E>, { MAX_SCHEDULED_EVENTS }>,
+    pub(crate) schedule_counter: u32,
+}
+
+impl<E: Debug> EventScheduler<E> {
+    pub const fn new() -> Self {
+        Self {
+            pending_events: heapless::spsc::Queue::new(),
+            scheduled_events: heapless::Vec::new(),
+            schedule_counter: 0,
+        }
+    }
+
+    pub fn init(&mut self) {
+        while self.pending_events.dequeue().is_some() {}
+        self.scheduled_events.clear();
+        self.schedule_counter = 0;
+    }
+
+    pub fn schedule_event(&mut self, scheduled_event: key::ScheduledEvent<E>) {
+        match scheduled_event.schedule {
+            key::Schedule::Immediate => {
+                self.enqueue_event(scheduled_event.event);
+            }
+            key::Schedule::After(delay) => {
+                self.schedule_after(delay as u32, scheduled_event.event);
+            }
+        }
+    }
+
+    pub fn enqueue_event(&mut self, event: Event<E>) {
+        self.pending_events.enqueue(event).unwrap();
+    }
+
+    pub fn schedule_after(&mut self, delay: u32, event: Event<E>) {
+        let time = self.schedule_counter + delay;
+        // binary sort insertion;
+        //  smallest at *end* (quick to pop off),
+        //  highest at *start*.
+        let pos = self
+            .scheduled_events
+            .binary_search_by(|sch_item| sch_item.time.cmp(&delay).reverse())
+            .unwrap_or_else(|e| e);
+        self.scheduled_events
+            .insert(pos, ScheduledEvent { time, event })
+            .unwrap();
+    }
+
+    pub fn cancel_events_for_keymap_index(&mut self, keymap_index: u16) {
+        self.scheduled_events
+            .retain(|ScheduledEvent { event, .. }| match event {
+                &Event::Key {
+                    keymap_index: ki, ..
+                } => ki != keymap_index,
+                _ => true,
+            });
+    }
+
+    pub fn tick(&mut self) {
+        self.schedule_counter += 1;
+        let scheduled_ready =
+            if let Some(&ScheduledEvent { time, .. }) = self.scheduled_events.last() {
+                time <= self.schedule_counter
+            } else {
+                false
+            };
+        if scheduled_ready {
+            if let Some(ScheduledEvent { event, .. }) = self.scheduled_events.pop() {
+                self.pending_events.enqueue(event).unwrap();
+            }
+        }
+    }
+
+    pub fn dequeue(&mut self) -> Option<Event<E>> {
+        self.pending_events.dequeue()
+    }
+}
