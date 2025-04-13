364d3fdf6d03ccf7486f9927dee7d7382e874700
Richard Goulter <richard.goulter@gmail.com>
2025-04-13 09:46:10 +0700

    stm32-embassy: remove example commentary

--- stm32-embassy-smart-keyboard/src/main.rs
+++ stm32-embassy-smart-keyboard/src/main.rs
@@ -21,11 +21,6 @@ bind_interrupts!(struct Irqs {
     OTG_FS => usb::InterruptHandler<peripherals::USB_OTG_FS>;
 });
 
-// If you are trying this and your USB device doesn't connect, the most
-// common issues are the RCC config and vbus_detection
-//
-// See https://embassy.dev/book/#_the_usb_examples_are_not_working_on_my_board_is_there_anything_else_i_need_to_configure
-// for more information.
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     let mut config = Config::default();
@@ -51,14 +46,9 @@ async fn main(_spawner: Spawner) {
     }
     let p = embassy_stm32::init(config);
 
-    // Create the driver, from the HAL.
     let mut ep_out_buffer = [0u8; 256];
     let mut config = embassy_stm32::usb::Config::default();
 
-    // Do not enable vbus_detection. This is a safe default that works in all boards.
-    // However, if your USB device is self-powered (can stay powered on if USB is unplugged), you need
-    // to enable vbus_detection to comply with the USB spec. If you enable it, the board
-    // has to support it or USB won't work at all. See docs on `vbus_detection` for details.
     config.vbus_detection = false;
 
     let driver = Driver::new_fs(
@@ -70,7 +60,6 @@ async fn main(_spawner: Spawner) {
         config,
     );
 
-    // Create embassy-usb Config
     let mut config = embassy_usb::Config::new(0xc0de, 0xcafe);
     config.manufacturer = Some("Embassy");
     config.product = Some("HID keyboard example");
@@ -78,12 +67,9 @@ async fn main(_spawner: Spawner) {
     config.max_power = 100;
     config.max_packet_size_0 = 64;
 
-    // Create embassy-usb DeviceBuilder using the driver and config.
-    // It needs some buffers for building the descriptors.
     let mut config_descriptor = [0; 256];
     let mut bos_descriptor = [0; 256];
-    // You can also add a Microsoft OS descriptor.
-    let mut msos_descriptor = [0; 256];
+    let mut ms_os_descriptor = [0; 256];
     let mut control_buf = [0; 64];
 
     let mut request_handler = MyRequestHandler {};
@@ -96,13 +82,12 @@ async fn main(_spawner: Spawner) {
         config,
         &mut config_descriptor,
         &mut bos_descriptor,
-        &mut msos_descriptor,
+        &mut ms_os_descriptor,
         &mut control_buf,
     );
 
     builder.handler(&mut device_handler);
 
-    // Create classes on the builder.
     let config = embassy_usb::class::hid::Config {
         report_descriptor: KeyboardReport::desc(),
         request_handler: None,
@@ -112,37 +97,30 @@ async fn main(_spawner: Spawner) {
 
     let hid = HidReaderWriter::<_, 1, 8>::new(&mut builder, &mut state, config);
 
-    // Build the builder.
     let mut usb = builder.build();
 
-    // Run the USB device.
     let usb_fut = usb.run();
 
     let (reader, mut writer) = hid.split();
 
     let mut button = ExtiInput::new(p.PA0, p.EXTI0, Pull::Up);
 
-    // Do stuff with the class!
     let in_fut = async {
         loop {
             button.wait_for_falling_edge().await;
-            // signal_pin.wait_for_high().await;
             info!("Button pressed!");
-            // Create a report with the A key pressed. (no shift modifier)
             let report = KeyboardReport {
                 keycodes: [4, 0, 0, 0, 0, 0],
                 leds: 0,
                 modifier: 0,
                 reserved: 0,
             };
-            // Send the report.
             match writer.write_serialize(&report).await {
                 Ok(()) => {}
                 Err(e) => warn!("Failed to send report: {:?}", e),
             };
 
             button.wait_for_rising_edge().await;
-            // signal_pin.wait_for_low().await;
             info!("Button released!");
             let report = KeyboardReport {
                 keycodes: [0, 0, 0, 0, 0, 0],
@@ -161,8 +139,6 @@ async fn main(_spawner: Spawner) {
         reader.run(false, &mut request_handler).await;
     };
 
-    // Run everything concurrently.
-    // If we had made everything `'static` above instead, we could do this using separate tasks instead.
     join(usb_fut, join(in_fut, out_fut)).await;
 }
 
