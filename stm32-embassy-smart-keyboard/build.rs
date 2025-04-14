e1bda4a03cc4c4f792b03b8baba8a6933f054e7a
Richard Goulter <richard.goulter@gmail.com>
2025-04-14 20:56:55 +0700

    stm32-embassy: build.rs: handle SMART_KEYBOARD_CUSTOM_BOARD

--- stm32-embassy-smart-keyboard/build.rs
+++ stm32-embassy-smart-keyboard/build.rs
@@ -1,4 +1,18 @@
+use std::env;
+
+use smart_keymap_nickel_helper::{
+    codegen_rust_module, nickel_board_rs_for_board_path, CodegenInputs,
+};
+
 fn main() {
     println!("cargo:rustc-link-arg-bins=--nmagic");
     println!("cargo:rustc-link-arg-bins=-Tdefmt.x");
+
+    codegen_rust_module(CodegenInputs {
+        env_var: "SMART_KEYBOARD_CUSTOM_BOARD",
+        cfg_name: "custom_board",
+        module_basename: "board.rs",
+        ncl_import_path: format!("{}/ncl", env!("CARGO_MANIFEST_DIR")).as_str(),
+        nickel_eval_fn: nickel_board_rs_for_board_path,
+    });
 }
