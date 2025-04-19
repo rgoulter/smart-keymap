3f2db596503444712daae36ec2ec792a9dbf294d
Richard Goulter <richard.goulter@gmail.com>
2025-04-19 20:38:13 +0700

    rp2040-rtic: use smart keymap nickel helper's codegen_rust_module

--- rp2040-rtic-smart-keyboard/build.rs
+++ rp2040-rtic-smart-keyboard/build.rs
@@ -1,48 +1,15 @@
 use std::env;
-use std::fs;
-use std::io::Write;
-use std::path::Path;
 
 use smart_keymap_nickel_helper::{
-    nickel_board_rs_for_board_path, rustfmt, NickelError, NickelEvalInputs,
+    codegen_rust_module, nickel_board_rs_for_board_path, CodegenInputs,
 };
 
 fn main() {
-    println!("cargo:rerun-if-env-changed=SMART_KEYBOARD_CUSTOM_BOARD");
-    println!("cargo::rustc-check-cfg=cfg(custom_board)");
-    if let Ok(custom_board_path) = env::var("SMART_KEYBOARD_CUSTOM_BOARD") {
-        let out_dir = env::var("OUT_DIR").unwrap();
-        let dest_path = Path::new(&out_dir).join("board.rs");
-        println!("cargo:rerun-if-changed={}", dest_path.to_str().unwrap());
-
-        if custom_board_path.ends_with(".rs") {
-            println!("cargo:rustc-cfg=custom_board");
-
-            // Copy the custom keymap file to the output directory
-            fs::copy(custom_board_path, &dest_path).unwrap();
-        } else if custom_board_path.ends_with(".ncl") {
-            println!("cargo:rustc-cfg=custom_board");
-
-            // Evaluate the custom keymap file with Nickel
-            let board_path = Path::new(&custom_board_path);
-            match nickel_board_rs_for_board_path(NickelEvalInputs {
-                ncl_import_path: format!("{}/ncl", env!("CARGO_MANIFEST_DIR")).as_str(),
-                input_path: board_path,
-            }) {
-                Ok(board_rs) => {
-                    let mut file = fs::File::create(&dest_path).unwrap();
-                    let formatted = rustfmt(board_rs);
-                    file.write_all(formatted.as_bytes()).unwrap();
-                }
-                Err(NickelError::NickelNotFound) => {
-                    panic!("`nickel` not found in PATH");
-                }
-                Err(NickelError::EvalError(e)) => {
-                    panic!("Nickel evaluation failed: {}", e);
-                }
-            }
-        } else {
-            panic!("Unsupported custom board path: {}", custom_board_path);
-        }
-    }
+    codegen_rust_module(CodegenInputs {
+        env_var: "SMART_KEYBOARD_CUSTOM_BOARD",
+        cfg_name: "custom_board",
+        module_basename: "board.rs",
+        ncl_import_path: format!("{}/ncl", env!("CARGO_MANIFEST_DIR")).as_str(),
+        nickel_eval_fn: nickel_board_rs_for_board_path,
+    });
 }
