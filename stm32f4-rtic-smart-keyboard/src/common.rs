7019d2bed15b4f274f8cd27cecf5015d440ee5dc
Richard Goulter <richard.goulter@gmail.com>
2025-04-19 08:46:43 +0700

    stm32f4-rtic: remove wildcard import from usbd_smart_keyboard

--- stm32f4-rtic-smart-keyboard/src/common.rs
+++ stm32f4-rtic-smart-keyboard/src/common.rs
@@ -1,7 +1,5 @@
 use stm32f4xx_hal::otg_fs::UsbBusType;
 
-pub use usbd_smart_keyboard::common::*;
-
 pub type UsbClass = usbd_smart_keyboard::common::UsbClass<UsbBusType>;
 
 pub type UsbDevice = usb_device::device::UsbDevice<'static, UsbBusType>;
