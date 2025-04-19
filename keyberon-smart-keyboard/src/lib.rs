f199b36a77b9dc9f726453868ac3a29acb9a3c58
Richard Goulter <richard.goulter@gmail.com>
2025-04-19 15:44:47 +0700

    keyberon_smart_keyboard: docs: add rough sketch for migrating keyberon code

--- keyberon-smart-keyboard/src/lib.rs
+++ keyberon-smart-keyboard/src/lib.rs
@@ -5,6 +5,41 @@
 //!
 //! See the `examples/` under `rp2040-rtic-smart-keyboard`
 //!  for an example of Rust firmware using this crate.
+//!
+//! ## Migrating from Keyberon
+//!
+//! Roughly, where Keyberon code is typically:
+//!
+//! ```ignore
+//! // Gather events from keyboard matrix
+//! let key_presses = matrix.get().unwrap();
+//! let debounced_events = debouncer.events(key_presses).collect();
+//! chording.tick(debounced_events)
+//!
+//! // Compute key codes from those events
+//! for event in chording.events() {
+//!     layout.event(event);
+//! }
+//! let keycodes = layout.keycodes();
+//! ```
+//!
+//! With this crate, [input::Keyboard] manages scanning/debouncing events
+//!  from the keyboard matrix. Chording is handled by smart keymap.
+//!
+//! For computing the key codes, this uses [input::smart_keymap::KeyboardBackend]:
+//!
+//! ```ignore
+//! // Compute key codes from those events
+//! for event in keyboard.events() {
+//!     if let Some(event) = keymap_index_of(&KEYMAP_INDICES, event) {
+//!         backend.event(event);
+//!     }
+//! }
+//! backend.tick();
+//! let key_codes = backend
+//!   .keymap_output()
+//!   .pressed_key_codes();
+//! ```
 
 #![no_main]
 #![no_std]
