20785b9a67e1efcfb996c55ea2f9e292202dd031
Richard Goulter <richard.goulter@gmail.com>
2025-04-19 08:57:47 +0700

    rp2040-rtic: inline use of UsbClass type alias

--- rp2040-rtic-smart-keyboard/src/common.rs
+++ rp2040-rtic-smart-keyboard/src/common.rs
@@ -1,8 +1,22 @@
-use hal::usb::UsbBus;
 use rp2040_hal as hal;
 
-/// Alias for [usbd_smart_keyboard::common::UsbClass] using the RP2040 [UsbBus].
-pub type UsbClass = usbd_smart_keyboard::common::UsbClass<UsbBus>;
+use frunk::HList;
+
+use usbd_human_interface_device::device::consumer::ConsumerControl;
+use usbd_human_interface_device::device::keyboard::NKROBootKeyboard;
+use usbd_human_interface_device::usb_class::UsbHidClass;
+
+use hal::usb::UsbBus;
+
+/// A [usb_device::class::UsbClass] impl. with HID Keyboard, HID consumer devices.
+pub type UsbClass = UsbHidClass<
+    'static,
+    UsbBus,
+    HList!(
+        ConsumerControl<'static, UsbBus>,
+        NKROBootKeyboard<'static, UsbBus>,
+    ),
+>;
 
 /// Alias for [usb_device::device::UsbDevice] using the RP2040 [UsbBus].
 pub type UsbDevice = usb_device::device::UsbDevice<'static, UsbBus>;
