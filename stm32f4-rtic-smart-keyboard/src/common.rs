e3295aa9d0d09292c0b613077b6349a623055908
Richard Goulter <richard.goulter@gmail.com>
2025-04-19 09:00:00 +0700

    stm32f4-rtic: inline use of UsbClass type alias

--- stm32f4-rtic-smart-keyboard/src/common.rs
+++ stm32f4-rtic-smart-keyboard/src/common.rs
@@ -1,5 +1,19 @@
+use frunk::HList;
+
 use stm32f4xx_hal::otg_fs::UsbBusType;
 
-pub type UsbClass = usbd_smart_keyboard::common::UsbClass<UsbBusType>;
+use usbd_human_interface_device::device::consumer::ConsumerControl;
+use usbd_human_interface_device::device::keyboard::NKROBootKeyboard;
+use usbd_human_interface_device::usb_class::UsbHidClass;
+
+/// A [usb_device::class::UsbClass] impl. with HID Keyboard, HID consumer devices.
+pub type UsbClass = UsbHidClass<
+    'static,
+    UsbBusType,
+    HList!(
+        ConsumerControl<'static, UsbBusType>,
+        NKROBootKeyboard<'static, UsbBusType>,
+    ),
+>;
 
 pub type UsbDevice = usb_device::device::UsbDevice<'static, UsbBusType>;
