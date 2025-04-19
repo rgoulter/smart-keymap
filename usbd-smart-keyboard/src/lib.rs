152e0b03a224461f72f5f461cede35f1d3a24eaf
Richard Goulter <richard.goulter@gmail.com>
2025-04-19 14:55:11 +0700

    usbd-smart-keyboard: remove app_prelude, common

--- usbd-smart-keyboard/src/lib.rs
+++ usbd-smart-keyboard/src/lib.rs
@@ -9,9 +9,6 @@
 #![no_main]
 #![no_std]
 
-/// Items which are useful to have imported in usbd keyboard firmware.
-pub mod app_prelude;
-
 /// Useful traits and structs related to input.
 pub mod input;
 
@@ -20,6 +17,3 @@ pub mod matrix;
 
 /// UART-based split pressed key communication.
 pub mod split;
-
-/// Common USB device/class definitions.
-pub mod common;
