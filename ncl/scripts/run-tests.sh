2283c45716248c39416ca95fcbaf5e837a6f032a
Richard Goulter <richard.goulter@gmail.com>
2025-04-29 22:05:40 +0700

    tests: ncl: add keymap-1key-custom to snapshot suite

--- ncl/scripts/run-tests.sh
+++ ncl/scripts/run-tests.sh
@@ -18,6 +18,7 @@ ncl_tests=(
     "keymap-1key-simple"
     "keymap-1key-tap_dance"
     "keymap-1key-tap_hold"
+    "keymap-1key-custom"
     "keymap-1key-2layer-th-lmod"
     "keymap-1key-callback-custom"
     "keymap-2key-2layer-simple"
