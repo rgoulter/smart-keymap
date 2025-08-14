2b369e08b8f158f58f47cdbcabea0fe78a636f9a
Richard Goulter <richard.goulter@gmail.com>
2025-08-14 20:37:02 +0700

    tests: rust: add motivating integration test

--- tests/rust/chorded/overlapping.rs
+++ tests/rust/chorded/overlapping.rs
@@ -101,6 +101,35 @@ fn overlap_tap_key_acts_as_passthrough() {
 }
 
 #[test]
+fn overlap_press_d_bc_results_in_passthrough_followed_by_chord() {
+    // Assemble
+    let mut keymap = Keymap::new(KEYS, CONTEXT);
+    let mut actual_reports = DistinctReports::new();
+
+    // Act
+    // Press D, then B and C.
+    let press_indices = &[3, 1, 2];
+
+    for &keymap_index in press_indices {
+        keymap.handle_input(input::Event::Press { keymap_index });
+        actual_reports.update(keymap.report_output().as_hid_boot_keyboard_report());
+    }
+
+    while keymap.has_scheduled_events() {
+        keymap.tick();
+        actual_reports.update(keymap.report_output().as_hid_boot_keyboard_report());
+    }
+
+    // Assert
+    let expected_reports: &[[u8; 8]] = &[
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0x07, 0, 0, 0, 0, 0],
+        [0, 0, 0x07, 0x12, 0, 0, 0, 0],
+    ];
+    assert_eq!(expected_reports, actual_reports.reports());
+}
+
+#[test]
 fn overlap_partial_press_cd_acts_as_chord() {
     // Assemble
     let mut keymap = Keymap::new(KEYS, CONTEXT);
