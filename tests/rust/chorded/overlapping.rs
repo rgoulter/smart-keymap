37f3f29c5aee03081d3a5c46a59b4b9cdb17d0a9
Richard Goulter <richard.goulter@gmail.com>
2025-08-14 16:14:27 +0700

    tests: rust: add motivating integration tests (fully overlapped)

--- tests/rust/chorded/overlapping.rs
+++ tests/rust/chorded/overlapping.rs
@@ -197,6 +197,126 @@ fn overlap_press_cb_acts_as_chord() {
 }
 
 #[test]
+fn overlap_press_ab_acts_as_chord() {
+    // Assemble
+    let mut keymap = Keymap::new(KEYS, CONTEXT);
+    let mut actual_reports = DistinctReports::new();
+
+    // Act
+    let press_indices = &[0, 1];
+
+    for &keymap_index in press_indices {
+        keymap.handle_input(input::Event::Press { keymap_index });
+        actual_reports.update(keymap.report_output().as_hid_boot_keyboard_report());
+    }
+
+    while keymap.has_scheduled_events() {
+        keymap.tick();
+        actual_reports.update(keymap.report_output().as_hid_boot_keyboard_report());
+    }
+
+    // Assert
+    let expected_reports: &[[u8; 8]] = &[[0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0x10, 0, 0, 0, 0, 0]];
+    assert_eq!(expected_reports, actual_reports.reports());
+}
+
+#[test]
+fn overlap_press_ba_acts_as_chord() {
+    // Assemble
+    let mut keymap = Keymap::new(KEYS, CONTEXT);
+    let mut actual_reports = DistinctReports::new();
+
+    // Act
+    let press_indices = &[1, 0];
+
+    for &keymap_index in press_indices {
+        keymap.handle_input(input::Event::Press { keymap_index });
+        actual_reports.update(keymap.report_output().as_hid_boot_keyboard_report());
+    }
+
+    while keymap.has_scheduled_events() {
+        keymap.tick();
+        actual_reports.update(keymap.report_output().as_hid_boot_keyboard_report());
+    }
+
+    // Assert
+    let expected_reports: &[[u8; 8]] = &[[0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0x10, 0, 0, 0, 0, 0]];
+    assert_eq!(expected_reports, actual_reports.reports());
+}
+
+#[test]
+fn overlap_press_abc_acts_as_chord() {
+    // Assemble
+    let mut keymap = Keymap::new(KEYS, CONTEXT);
+    let mut actual_reports = DistinctReports::new();
+
+    // Act
+    let press_indices = &[0, 1, 2];
+
+    for &keymap_index in press_indices {
+        keymap.handle_input(input::Event::Press { keymap_index });
+        actual_reports.update(keymap.report_output().as_hid_boot_keyboard_report());
+    }
+
+    while keymap.has_scheduled_events() {
+        keymap.tick();
+        actual_reports.update(keymap.report_output().as_hid_boot_keyboard_report());
+    }
+
+    // Assert
+    let expected_reports: &[[u8; 8]] = &[[0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0x11, 0, 0, 0, 0, 0]];
+    assert_eq!(expected_reports, actual_reports.reports());
+}
+
+#[test]
+fn overlap_press_cba_acts_as_chord() {
+    // Assemble
+    let mut keymap = Keymap::new(KEYS, CONTEXT);
+    let mut actual_reports = DistinctReports::new();
+
+    // Act
+    let press_indices = &[2, 1, 0];
+
+    for &keymap_index in press_indices {
+        keymap.handle_input(input::Event::Press { keymap_index });
+        actual_reports.update(keymap.report_output().as_hid_boot_keyboard_report());
+    }
+
+    while keymap.has_scheduled_events() {
+        keymap.tick();
+        actual_reports.update(keymap.report_output().as_hid_boot_keyboard_report());
+    }
+
+    // Assert
+    let expected_reports: &[[u8; 8]] = &[[0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0x11, 0, 0, 0, 0, 0]];
+    assert_eq!(expected_reports, actual_reports.reports());
+}
+
+#[test]
+fn overlap_press_cab_acts_as_chord() {
+    // Assemble
+    let mut keymap = Keymap::new(KEYS, CONTEXT);
+    let mut actual_reports = DistinctReports::new();
+
+    // Act
+    let press_indices = &[2, 0, 1];
+
+    for &keymap_index in press_indices {
+        keymap.handle_input(input::Event::Press { keymap_index });
+        actual_reports.update(keymap.report_output().as_hid_boot_keyboard_report());
+    }
+
+    while keymap.has_scheduled_events() {
+        keymap.tick();
+        actual_reports.update(keymap.report_output().as_hid_boot_keyboard_report());
+    }
+
+    // Assert
+    let expected_reports: &[[u8; 8]] = &[[0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0x11, 0, 0, 0, 0, 0]];
+    assert_eq!(expected_reports, actual_reports.reports());
+}
+
+#[test]
 fn interrupting_satisfied_overlapped_chord_resolves_as_chord() {
     // Assemble
     let mut keymap = Keymap::new(KEYS, CONTEXT);
