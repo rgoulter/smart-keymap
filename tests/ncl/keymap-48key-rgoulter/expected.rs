0ee0b8182d2ebb622159722fe6518d25479370c8
Richard Goulter <richard.goulter@gmail.com>
2025-08-16 11:34:17 +0700

    ncl: keymap-codegen: update key::chorded::Config chords

--- tests/ncl/keymap-48key-rgoulter/expected.rs
+++ tests/ncl/keymap-48key-rgoulter/expected.rs
@@ -3,10 +3,10 @@ pub mod init {
     /// Config used by tap-hold keys.
     pub const CONFIG: crate::key::composite::Config = crate::key::composite::Config {
         chorded: crate::key::chorded::Config {
-            chords: [
-                Some(crate::key::chorded::ChordIndices::from_slice(&[26, 27])),
-                Some(crate::key::chorded::ChordIndices::from_slice(&[32, 33])),
-            ],
+            chords: crate::slice::Slice::from_slice(&[
+                crate::key::chorded::ChordIndices::from_slice(&[26, 27]),
+                crate::key::chorded::ChordIndices::from_slice(&[32, 33]),
+            ]),
             ..crate::key::chorded::DEFAULT_CONFIG
         },
         sticky: crate::key::sticky::DEFAULT_CONFIG,
