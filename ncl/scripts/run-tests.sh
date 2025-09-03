491650a2a4c19d491747406c6491282219d72d67
Richard Goulter <richard.goulter@gmail.com>
2025-09-03 11:52:02 +0700

    TMP: tests: ncl: remove snapshot tests

--- ncl/scripts/run-tests.sh
+++ ncl/scripts/run-tests.sh
@@ -16,19 +16,6 @@ SCRIPTS_DIR="$(dirname "$0")"
 #  - can be compiled.
 ncl_tests=(
     "keymap-1key-simple"
-    "keymap-1key-tap_dance"
-    "keymap-1key-tap_hold"
-    "keymap-1key-custom"
-    "keymap-1key-2layer-th-lmod"
-    "keymap-1key-callback-custom"
-    "keymap-2key-2layer-simple"
-    "keymap-2key-2layer-composite"
-    "keymap-2key-chorded"
-    "keymap-48key-basic"
-    "keymap-48key-rgoulter"
-    "keymap-60key-dvorak-simple"
-    "keymap-60key-dvorak-simple-with-tap_hold"
-    "keymap-34key-seniply"
 )
 for ncl_test in "${ncl_tests[@]}"
 do
