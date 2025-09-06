0a7c4e284f483672333ab0e0c91931a3b13b4290
Richard Goulter <richard.goulter@gmail.com>
2025-09-06 16:24:42 +0700

    iterate: integ., codegen: get trivial test to pass

--- tests/rust/keymap.rs
+++ tests/rust/keymap.rs
@@ -70,45 +70,45 @@ fn basic_keymap_expression() {
     assert_eq!(expected_reports, actual_reports.reports());
 }
 
-// #[test]
-// fn basic_keymap_expression_macro() {
-//     // This test demonstrates using smart_keymap::keymap::Keymap directly,
-//     //  by using the keymap! macro.
+#[test]
+fn basic_keymap_expression_macro() {
+    // This test demonstrates using smart_keymap::keymap::Keymap directly,
+    //  by using the keymap! macro.
 
-//     // Assemble
-//     use smart_keymap::input;
-//     use smart_keymap::keymap;
+    // Assemble
+    use smart_keymap::input;
+    use smart_keymap::keymap;
 
-//     use keymap::DistinctReports;
+    use keymap::DistinctReports;
 
-//     let mut keymap = smart_keymap_macros::keymap!(
-//         r#"
-//         {
-//             keys = [
-//                 { key_code = 4 },
-//             ],
-//         }
-//         "#
-//     );
-//     let mut actual_reports = DistinctReports::new();
+    let mut keymap = smart_keymap_macros::keymap!(
+        r#"
+        {
+            keys = [
+                { key_code = 4 },
+            ],
+        }
+        "#
+    );
+    let mut actual_reports = DistinctReports::new();
 
-//     // Act -- tap 'a'
-//     keymap.handle_input(input::Event::Press { keymap_index: 0 });
-//     actual_reports.update(keymap.report_output().as_hid_boot_keyboard_report());
-//     keymap.handle_input(input::Event::Release { keymap_index: 0 });
-//     actual_reports.update(keymap.report_output().as_hid_boot_keyboard_report());
+    // Act -- tap 'a'
+    keymap.handle_input(input::Event::Press { keymap_index: 0 });
+    actual_reports.update(keymap.report_output().as_hid_boot_keyboard_report());
+    keymap.handle_input(input::Event::Release { keymap_index: 0 });
+    actual_reports.update(keymap.report_output().as_hid_boot_keyboard_report());
 
-//     while keymap.has_scheduled_events() {
-//         keymap.tick();
-//         actual_reports.update(keymap.report_output().as_hid_boot_keyboard_report());
-//     }
+    while keymap.has_scheduled_events() {
+        keymap.tick();
+        actual_reports.update(keymap.report_output().as_hid_boot_keyboard_report());
+    }
 
-//     // Assert -- tap-hold key immediately resolves as 'tap'
-//     #[rustfmt::skip]
-//     let expected_reports: &[[u8; 8]] = &[
-//         [0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 0, 0x04, 0, 0, 0, 0, 0],
-//         [0, 0, 0, 0, 0, 0, 0, 0],
-//     ];
-//     assert_eq!(expected_reports, actual_reports.reports());
-// }
+    // Assert -- tap-hold key immediately resolves as 'tap'
+    #[rustfmt::skip]
+    let expected_reports: &[[u8; 8]] = &[
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0x04, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+    ];
+    assert_eq!(expected_reports, actual_reports.reports());
+}
