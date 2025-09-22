dd45bdd7920154fa0c4fbae293b982b05d769c56
Richard Goulter <richard.goulter@gmail.com>
2025-09-22 12:02:26 +0700

    features: update generate-doc.sh to include key fields

--- features/scripts/generate-doc.sh
+++ features/scripts/generate-doc.sh
@@ -8,6 +8,8 @@ REPOSITORY_DIR="${SCRIPTS_DIR}/../.."
 FEATURES_DIR="${REPOSITORY_DIR}/features"
 KEYMAP_FEATURES_DIR="${FEATURES_DIR}/keymap"
 
+NCL_DIR="${REPOSITORY_DIR}/ncl"
+
 KEYMAP_FEATURE_MD=""
 
 keymap_key_features=(
@@ -32,6 +34,13 @@ keymap_ncl_features=(
     "layer_string"
     "chords"
 )
+
+KEY_FIELDS_MD="${NCL_DIR}/key-docs.md"
+nickel export \
+    --field=keys_md \
+    --format=text \
+    "${NCL_DIR}/key-docs.ncl" \
+    > "${KEY_FIELDS_MD}"
 
 KEYMAP_KEY_FEATURES_DIR="${KEYMAP_FEATURES_DIR}/key"
 KEYMAP_FEATURE_MD="${KEYMAP_FEATURE_MD} ${KEYMAP_KEY_FEATURES_DIR}/readme.md"
@@ -65,4 +74,5 @@ pandoc \
   --toc-depth=4 \
   --metadata=title="Smart Keymap Features" \
   ${KEYMAP_FEATURE_MD} \
+  ${KEY_FIELDS_MD} \
   --output="${FEATURES_DIR}/generated-features.html"
