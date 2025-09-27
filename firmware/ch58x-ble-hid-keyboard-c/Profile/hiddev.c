a7a8b704dc89997d195bee43a6253b0115656d19
Richard Goulter <richard.goulter@gmail.com>
2025-09-27 11:31:04 +0700

    firmware: ch58x: add carriage return to more PRINT

--- firmware/ch58x-ble-hid-keyboard-c/Profile/hiddev.c
+++ firmware/ch58x-ble-hid-keyboard-c/Profile/hiddev.c
@@ -609,12 +609,12 @@ static void hidDevProcessGAPMsg(gapRoleEvent_t *pEvent) {
     PRINT("recv scan Req addr ");
     for (int i = 0; i < B_ADDR_LEN; i++)
       PRINT("%02x ", pEvent->scanReqEvt.scannerAddr[i]);
-    PRINT("\n");
+    PRINT("\r\n");
     break;
   }
 
   case GAP_PHY_UPDATE_EVENT: {
-    PRINT("Phy update Rx:%x Tx:%x ..\n", pEvent->linkPhyUpdate.connRxPHYS,
+    PRINT("Phy update Rx:%x Tx:%x ..\r\n", pEvent->linkPhyUpdate.connRxPHYS,
           pEvent->linkPhyUpdate.connTxPHYS);
     break;
   }
@@ -745,7 +745,7 @@ static void hidDevGapStateCB(gapRole_States_t newState,
 static void hidDevParamUpdateCB(uint16_t connHandle, uint16_t connInterval,
                                 uint16_t connSlaveLatency,
                                 uint16_t connTimeout) {
-  PRINT("Update %d - Int 0x%x - Latency %d\n", connHandle, connInterval,
+  PRINT("Update %d - Int 0x%x - Latency %d\r\n", connHandle, connInterval,
         connSlaveLatency);
 }
 
