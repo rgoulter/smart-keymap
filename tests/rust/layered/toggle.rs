88255f9219d0dd3139971b66327fff2dcb944db1
Richard Goulter <richard.goulter@gmail.com>
2025-10-02 17:37:29 +0700

    tests: rust: rewrite key::layered tests to use ObservedKeymap

--- tests/rust/layered/toggle.rs
+++ tests/rust/layered/toggle.rs
@@ -1,11 +1,12 @@
 use smart_keymap::input;
+use smart_keymap::keymap::ObservedKeymap;
 
 use smart_keymap_macros::keymap;
 
 #[test]
 fn press_active_layer_when_layer_mod_toggle_pressed() {
     // Assemble
-    let mut keymap = keymap!(
+    let mut keymap = ObservedKeymap::new(keymap!(
         r#"
             let K = import "keys.ncl" in
             {
@@ -15,24 +16,22 @@ fn press_active_layer_when_layer_mod_toggle_pressed() {
                 ],
             }
         "#
-    );
+    ));
 
     // Act
     keymap.handle_input(input::Event::Press { keymap_index: 0 });
-    keymap.tick();
     keymap.handle_input(input::Event::Press { keymap_index: 1 });
-    keymap.tick();
-    let actual_report = keymap.boot_keyboard_report();
 
     // Assert
     let expected_report: [u8; 8] = [0, 0, 0x05, 0, 0, 0, 0, 0];
+    let actual_report = keymap.boot_keyboard_report();
     assert_eq!(expected_report, actual_report);
 }
 
 #[test]
 fn press_active_layer_when_layer_mod_toggle_tapped() {
     // Assemble
-    let mut keymap = keymap!(
+    let mut keymap = ObservedKeymap::new(keymap!(
         r#"
             let K = import "keys.ncl" in
             {
@@ -42,26 +41,23 @@ fn press_active_layer_when_layer_mod_toggle_tapped() {
                 ],
             }
         "#
-    );
+    ));
 
     // Act
     keymap.handle_input(input::Event::Press { keymap_index: 0 });
-    keymap.tick();
     keymap.handle_input(input::Event::Release { keymap_index: 0 });
-    keymap.tick();
     keymap.handle_input(input::Event::Press { keymap_index: 1 });
-    keymap.tick();
-    let actual_report = keymap.boot_keyboard_report();
 
     // Assert
     let expected_report: [u8; 8] = [0, 0, 0x05, 0, 0, 0, 0, 0];
+    let actual_report = keymap.boot_keyboard_report();
     assert_eq!(expected_report, actual_report);
 }
 
 #[test]
 fn toggle_tapped_twice_deactivates_layer() {
     // Assemble
-    let mut keymap = keymap!(
+    let mut keymap = ObservedKeymap::new(keymap!(
         r#"
             let K = import "keys.ncl" in
             {
@@ -71,22 +67,17 @@ fn toggle_tapped_twice_deactivates_layer() {
                 ],
             }
         "#
-    );
+    ));
 
     // Act
     keymap.handle_input(input::Event::Press { keymap_index: 0 });
-    keymap.tick();
     keymap.handle_input(input::Event::Release { keymap_index: 0 });
-    keymap.tick();
     keymap.handle_input(input::Event::Press { keymap_index: 0 });
-    keymap.tick();
     keymap.handle_input(input::Event::Release { keymap_index: 0 });
-    keymap.tick();
     keymap.handle_input(input::Event::Press { keymap_index: 1 });
-    keymap.tick();
-    let actual_report = keymap.boot_keyboard_report();
 
     // Assert
     let expected_report: [u8; 8] = [0, 0, 0x04, 0, 0, 0, 0, 0];
+    let actual_report = keymap.boot_keyboard_report();
     assert_eq!(expected_report, actual_report);
 }
