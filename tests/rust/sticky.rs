e2337a0531d1ec7778ed49568bd50a3baa00aacf
Richard Goulter <richard.goulter@gmail.com>
2025-10-02 17:41:32 +0700

    tests: rust: rewrite key::sticky tests to use ObservedKeymap

--- tests/rust/sticky.rs
+++ tests/rust/sticky.rs
@@ -1,16 +1,14 @@
 mod release_on_next_press;
 
 use smart_keymap::input;
-use smart_keymap::keymap;
+use smart_keymap::keymap::ObservedKeymap;
 
 use smart_keymap_macros::keymap;
-
-use keymap::DistinctReports;
 
 #[test]
 fn tap_sticky_mod_modifies_next_keyboard_key() {
     // Assemble
-    let mut keymap = keymap!(
+    let mut keymap = ObservedKeymap::new(keymap!(
         r#"
             let K = import "keys.ncl" in
             {
@@ -22,24 +20,17 @@ fn tap_sticky_mod_modifies_next_keyboard_key() {
                 ],
             }
         "#
-    );
-    let mut actual_reports = DistinctReports::new();
+    ));
 
     // Act
     // Tap Sticky Modifier
     keymap.handle_input(input::Event::Press { keymap_index: 0 });
-    actual_reports.update(keymap.report_output().as_hid_boot_keyboard_report());
     keymap.handle_input(input::Event::Release { keymap_index: 0 });
-    actual_reports.update(keymap.report_output().as_hid_boot_keyboard_report());
 
     // Press "A"
     keymap.handle_input(input::Event::Press { keymap_index: 1 });
-    actual_reports.update(keymap.report_output().as_hid_boot_keyboard_report());
 
-    while keymap.has_scheduled_events() {
-        keymap.tick();
-        actual_reports.update(keymap.report_output().as_hid_boot_keyboard_report());
-    }
+    keymap.tick_until_no_scheduled_events();
 
     // Assert
     let expected_reports: &[[u8; 8]] = &[
@@ -47,13 +38,14 @@ fn tap_sticky_mod_modifies_next_keyboard_key() {
         [0x02, 0, 0, 0, 0, 0, 0, 0],
         [0x02, 0, 0x04, 0, 0, 0, 0, 0],
     ];
+    let actual_reports = keymap.distinct_reports();
     assert_eq!(expected_reports, actual_reports.reports());
 }
 
 #[test]
 fn tap_sticky_mod_acts_as_regular_mod_when_interrupted_by_key_slash() {
     // Assemble
-    let mut keymap = keymap!(
+    let mut keymap = ObservedKeymap::new(keymap!(
         r#"
             let K = import "keys.ncl" in
             {
@@ -65,24 +57,17 @@ fn tap_sticky_mod_acts_as_regular_mod_when_interrupted_by_key_slash() {
                 ],
             }
         "#
-    );
-    let mut actual_reports = DistinctReports::new();
+    ));
 
     // Act
     // Tap Sticky Modifier
     keymap.handle_input(input::Event::Press { keymap_index: 0 });
-    actual_reports.update(keymap.report_output().as_hid_boot_keyboard_report());
     keymap.handle_input(input::Event::Release { keymap_index: 0 });
-    actual_reports.update(keymap.report_output().as_hid_boot_keyboard_report());
 
     // Press "/"
     keymap.handle_input(input::Event::Press { keymap_index: 2 });
-    actual_reports.update(keymap.report_output().as_hid_boot_keyboard_report());
 
-    while keymap.has_scheduled_events() {
-        keymap.tick();
-        actual_reports.update(keymap.report_output().as_hid_boot_keyboard_report());
-    }
+    keymap.tick_until_no_scheduled_events();
 
     // Assert
     let expected_reports: &[[u8; 8]] = &[
@@ -90,13 +75,14 @@ fn tap_sticky_mod_acts_as_regular_mod_when_interrupted_by_key_slash() {
         [0x02, 0, 0, 0, 0, 0, 0, 0],
         [0x02, 0, 0x38, 0, 0, 0, 0, 0],
     ];
+    let actual_reports = keymap.distinct_reports();
     assert_eq!(expected_reports, actual_reports.reports());
 }
 
 #[test]
 fn tap_sticky_mod_modifies_only_next_keyboard_key() {
     // Assemble
-    let mut keymap = keymap!(
+    let mut keymap = ObservedKeymap::new(keymap!(
         r#"
             let K = import "keys.ncl" in
             {
@@ -108,30 +94,21 @@ fn tap_sticky_mod_modifies_only_next_keyboard_key() {
                 ],
             }
         "#
-    );
-    let mut actual_reports = DistinctReports::new();
+    ));
 
     // Act
     // Tap Sticky Modifier
     keymap.handle_input(input::Event::Press { keymap_index: 0 });
-    actual_reports.update(keymap.report_output().as_hid_boot_keyboard_report());
     keymap.handle_input(input::Event::Release { keymap_index: 0 });
-    actual_reports.update(keymap.report_output().as_hid_boot_keyboard_report());
 
     // Tap "A"
     keymap.handle_input(input::Event::Press { keymap_index: 1 });
-    actual_reports.update(keymap.report_output().as_hid_boot_keyboard_report());
     keymap.handle_input(input::Event::Release { keymap_index: 1 });
-    actual_reports.update(keymap.report_output().as_hid_boot_keyboard_report());
 
     // Press "/"
     keymap.handle_input(input::Event::Press { keymap_index: 2 });
-    actual_reports.update(keymap.report_output().as_hid_boot_keyboard_report());
 
-    while keymap.has_scheduled_events() {
-        keymap.tick();
-        actual_reports.update(keymap.report_output().as_hid_boot_keyboard_report());
-    }
+    keymap.tick_until_no_scheduled_events();
 
     // Assert
     let expected_reports: &[[u8; 8]] = &[
@@ -141,13 +118,14 @@ fn tap_sticky_mod_modifies_only_next_keyboard_key() {
         [0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0x38, 0, 0, 0, 0, 0],
     ];
+    let actual_reports = keymap.distinct_reports();
     assert_eq!(expected_reports, actual_reports.reports());
 }
 
 #[test]
 fn tap_sticky_mod_acts_as_regular_mod_when_interrupted_by_key() {
     // Assemble
-    let mut keymap = keymap!(
+    let mut keymap = ObservedKeymap::new(keymap!(
         r#"
             let K = import "keys.ncl" in
             {
@@ -159,30 +137,22 @@ fn tap_sticky_mod_acts_as_regular_mod_when_interrupted_by_key() {
                 ],
             }
         "#
-    );
-    let mut actual_reports = DistinctReports::new();
+    ));
 
     // Act
     // Press Sticky Modifier
     keymap.handle_input(input::Event::Press { keymap_index: 0 });
-    actual_reports.update(keymap.report_output().as_hid_boot_keyboard_report());
 
     // Press "A"
     keymap.handle_input(input::Event::Press { keymap_index: 1 });
-    actual_reports.update(keymap.report_output().as_hid_boot_keyboard_report());
 
     // Release Sticky Modifier
     keymap.handle_input(input::Event::Release { keymap_index: 0 });
-    actual_reports.update(keymap.report_output().as_hid_boot_keyboard_report());
 
     // Release "A"
     keymap.handle_input(input::Event::Release { keymap_index: 1 });
-    actual_reports.update(keymap.report_output().as_hid_boot_keyboard_report());
 
-    while keymap.has_scheduled_events() {
-        keymap.tick();
-        actual_reports.update(keymap.report_output().as_hid_boot_keyboard_report());
-    }
+    keymap.tick_until_no_scheduled_events();
 
     // Assert
     let expected_reports: &[[u8; 8]] = &[
@@ -191,13 +161,14 @@ fn tap_sticky_mod_acts_as_regular_mod_when_interrupted_by_key() {
         [0, 0, 0x04, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0],
     ];
+    let actual_reports = keymap.distinct_reports();
     assert_eq!(expected_reports, actual_reports.reports());
 }
 
 #[test]
 fn tap_multiple_sticky_mod_modifies_next_keyboard_key() {
     // Assemble
-    let mut keymap = keymap!(
+    let mut keymap = ObservedKeymap::new(keymap!(
         r#"
             let K = import "keys.ncl" in
             {
@@ -209,30 +180,20 @@ fn tap_multiple_sticky_mod_modifies_next_keyboard_key() {
                 ],
             }
         "#
-    );
-    let mut actual_reports = DistinctReports::new();
+    ));
 
     // Act
     // Tap Sticky Modifiers
     keymap.handle_input(input::Event::Press { keymap_index: 0 });
-    actual_reports.update(keymap.report_output().as_hid_boot_keyboard_report());
     keymap.handle_input(input::Event::Release { keymap_index: 0 });
-    actual_reports.update(keymap.report_output().as_hid_boot_keyboard_report());
     keymap.handle_input(input::Event::Press { keymap_index: 3 });
-    actual_reports.update(keymap.report_output().as_hid_boot_keyboard_report());
     keymap.handle_input(input::Event::Release { keymap_index: 3 });
-    actual_reports.update(keymap.report_output().as_hid_boot_keyboard_report());
 
     // Tap "A"
     keymap.handle_input(input::Event::Press { keymap_index: 1 });
-    actual_reports.update(keymap.report_output().as_hid_boot_keyboard_report());
     keymap.handle_input(input::Event::Release { keymap_index: 1 });
-    actual_reports.update(keymap.report_output().as_hid_boot_keyboard_report());
 
-    while keymap.has_scheduled_events() {
-        keymap.tick();
-        actual_reports.update(keymap.report_output().as_hid_boot_keyboard_report());
-    }
+    keymap.tick_until_no_scheduled_events();
 
     // Assert
     let expected_reports: &[[u8; 8]] = &[
@@ -242,13 +203,14 @@ fn tap_multiple_sticky_mod_modifies_next_keyboard_key() {
         [0x03, 0, 0x04, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0],
     ];
+    let actual_reports = keymap.distinct_reports();
     assert_eq!(expected_reports, actual_reports.reports());
 }
 
 #[test]
 fn tap_sticky_mod_modifies_next_keyboard_key_until_released() {
     // Assemble
-    let mut keymap = keymap!(
+    let mut keymap = ObservedKeymap::new(keymap!(
         r#"
             let K = import "keys.ncl" in
             {
@@ -260,34 +222,24 @@ fn tap_sticky_mod_modifies_next_keyboard_key_until_released() {
                 ],
             }
         "#
-    );
-    let mut actual_reports = DistinctReports::new();
+    ));
 
     // Act
     // Tap Sticky Modifier
     keymap.handle_input(input::Event::Press { keymap_index: 0 });
-    actual_reports.update(keymap.report_output().as_hid_boot_keyboard_report());
     keymap.handle_input(input::Event::Release { keymap_index: 0 });
-    actual_reports.update(keymap.report_output().as_hid_boot_keyboard_report());
 
     // Press "A"
     keymap.handle_input(input::Event::Press { keymap_index: 1 });
-    actual_reports.update(keymap.report_output().as_hid_boot_keyboard_report());
 
     // Tap "/"
     keymap.handle_input(input::Event::Press { keymap_index: 2 });
-    actual_reports.update(keymap.report_output().as_hid_boot_keyboard_report());
     keymap.handle_input(input::Event::Release { keymap_index: 2 });
-    actual_reports.update(keymap.report_output().as_hid_boot_keyboard_report());
 
     // Release "A"
     keymap.handle_input(input::Event::Release { keymap_index: 1 });
-    actual_reports.update(keymap.report_output().as_hid_boot_keyboard_report());
 
-    while keymap.has_scheduled_events() {
-        keymap.tick();
-        actual_reports.update(keymap.report_output().as_hid_boot_keyboard_report());
-    }
+    keymap.tick_until_no_scheduled_events();
 
     // Assert
     let expected_reports: &[[u8; 8]] = &[
@@ -298,5 +250,6 @@ fn tap_sticky_mod_modifies_next_keyboard_key_until_released() {
         [0, 0, 0x04, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0],
     ];
+    let actual_reports = keymap.distinct_reports();
     assert_eq!(expected_reports, actual_reports.reports());
 }
