09a709b2637c8775b702a84aed2509c86ca398ee
Richard Goulter <richard.goulter@gmail.com>
2025-10-02 17:34:12 +0700

    tests: rust: rewrite key::layered tests to use ObservedKeymap

--- tests/rust/layered.rs
+++ tests/rust/layered.rs
@@ -3,13 +3,14 @@ mod tap_hold;
 mod toggle;
 
 use smart_keymap::input;
+use smart_keymap::keymap::ObservedKeymap;
 
 use smart_keymap_macros::keymap;
 
 #[test]
 fn press_base_key_when_no_layers_active() {
     // Assemble
-    let mut keymap = keymap!(
+    let mut keymap = ObservedKeymap::new(keymap!(
         r#"
             let K = import "keys.ncl" in
             {
@@ -19,21 +20,21 @@ fn press_base_key_when_no_layers_active() {
                 ],
             }
         "#
-    );
+    ));
 
     // Act
     keymap.handle_input(input::Event::Press { keymap_index: 1 });
-    let actual_report = keymap.boot_keyboard_report();
 
     // Assert
     let expected_report: [u8; 8] = [0, 0, 0x04, 0, 0, 0, 0, 0];
+    let actual_report = keymap.boot_keyboard_report();
     assert_eq!(expected_report, actual_report,);
 }
 
 #[test]
 fn press_active_layer_when_layer_mod_held() {
     // Assemble
-    let mut keymap = keymap!(
+    let mut keymap = ObservedKeymap::new(keymap!(
         r#"
             let K = import "keys.ncl" in
             {
@@ -43,25 +44,22 @@ fn press_active_layer_when_layer_mod_held() {
                 ],
             }
         "#
-    );
+    ));
 
     // Act
     keymap.handle_input(input::Event::Press { keymap_index: 0 });
-    for _ in 0..smart_keymap::keymap::INPUT_QUEUE_TICK_DELAY {
-        keymap.tick();
-    }
     keymap.handle_input(input::Event::Press { keymap_index: 1 });
-    let actual_report = keymap.boot_keyboard_report();
 
     // Assert
     let expected_report: [u8; 8] = [0, 0, 0x05, 0, 0, 0, 0, 0];
+    let actual_report = keymap.boot_keyboard_report();
     assert_eq!(expected_report, actual_report);
 }
 
 #[test]
 fn press_retained_when_layer_mod_released() {
     // Assemble
-    let mut keymap = keymap!(
+    let mut keymap = ObservedKeymap::new(keymap!(
         r#"
             let K = import "keys.ncl" in
             {
@@ -71,29 +69,23 @@ fn press_retained_when_layer_mod_released() {
                 ],
             }
         "#
-    );
+    ));
 
     // Act
     keymap.handle_input(input::Event::Press { keymap_index: 0 });
-    for _ in 0..smart_keymap::keymap::INPUT_QUEUE_TICK_DELAY {
-        keymap.tick();
-    }
     keymap.handle_input(input::Event::Press { keymap_index: 1 });
-    for _ in 0..smart_keymap::keymap::INPUT_QUEUE_TICK_DELAY {
-        keymap.tick();
-    }
     keymap.handle_input(input::Event::Release { keymap_index: 0 });
-    let actual_report = keymap.boot_keyboard_report();
 
     // Assert
     let expected_report: [u8; 8] = [0, 0, 0x05, 0, 0, 0, 0, 0];
+    let actual_report = keymap.boot_keyboard_report();
     assert_eq!(expected_report, actual_report);
 }
 
 #[test]
 fn uses_base_when_pressed_after_layer_mod_released() {
     // Assemble
-    let mut keymap = keymap!(
+    let mut keymap = ObservedKeymap::new(keymap!(
         r#"
             let K = import "keys.ncl" in
             {
@@ -103,21 +95,15 @@ fn uses_base_when_pressed_after_layer_mod_released() {
                 ],
             }
         "#
-    );
+    ));
 
     // Act
     keymap.handle_input(input::Event::Press { keymap_index: 0 });
-    for _ in 0..smart_keymap::keymap::INPUT_QUEUE_TICK_DELAY {
-        keymap.tick();
-    }
     keymap.handle_input(input::Event::Release { keymap_index: 0 });
-    for _ in 0..smart_keymap::keymap::INPUT_QUEUE_TICK_DELAY {
-        keymap.tick();
-    }
     keymap.handle_input(input::Event::Press { keymap_index: 1 });
-    let actual_report = keymap.boot_keyboard_report();
 
     // Assert
     let expected_report: [u8; 8] = [0, 0, 0x04, 0, 0, 0, 0, 0];
+    let actual_report = keymap.boot_keyboard_report();
     assert_eq!(expected_report, actual_report);
 }
