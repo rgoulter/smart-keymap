4b08349e6ad8a0921f7f4ec3b14e96563fcf63b5
Richard Goulter <richard.goulter@gmail.com>
2025-10-03 15:50:06 +0700

    tests: rust: add test for automation key: press, release only

--- tests/rust/automation.rs
+++ tests/rust/automation.rs
@@ -272,3 +272,52 @@ fn test_macro_press_while_pressed_release() {
     let actual_reports = keymap.distinct_reports();
     assert_eq!(expected_reports, actual_reports.reports());
 }
+
+#[test]
+fn test_macro_press_and_release() {
+    // Assemble
+    use smart_keymap::input;
+
+    let mut keymap = ObservedKeymap::new(smart_keymap_macros::keymap!(
+        r#"
+        let K = import "keys.ncl" in
+
+        let MY_MACRO = {
+            automation_instructions = {
+                on_press = [
+                    { Press = { key_code = { Keyboard = 0x04 } } },
+                    { Release = { key_code = { Keyboard = 0x04 } } },
+                ],
+                on_release = [
+                    { Press = { key_code = { Keyboard = 0x06 } } },
+                    { Release = { key_code = { Keyboard = 0x06 } } },
+                ],
+            }
+        }
+        in
+        {
+            keys = [
+                MY_MACRO,
+            ],
+        }
+        "#
+    ));
+
+    // Act -- press macro key; wait 3500
+    keymap.handle_input(input::Event::Press { keymap_index: 0 });
+    keymap.handle_input(input::Event::Release { keymap_index: 0 });
+
+    keymap.tick_until_no_scheduled_events();
+
+    // Assert -- the macro should have repeated 3 times
+    #[rustfmt::skip]
+    let expected_reports: &[[u8; 8]] = &[
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0x04, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0x06, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+    ];
+    let actual_reports = keymap.distinct_reports();
+    assert_eq!(expected_reports, actual_reports.reports());
+}
