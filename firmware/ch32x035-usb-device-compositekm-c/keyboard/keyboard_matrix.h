33ef6b0c1b88c8517972e56baad30bca86aed05e
Richard Goulter <richard.goulter@gmail.com>
2025-11-02 18:31:51 +0700

    firmware: ch32x: extract keyboard_matrix function declarations

--- firmware/ch32x035-usb-device-compositekm-c/keyboard/keyboard_matrix.h
+++ firmware/ch32x035-usb-device-compositekm-c/keyboard/keyboard_matrix.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "generated/keyboard_matrix.h" // IWYU pragma: export
+#include <stdbool.h>
 bool keyboard_matrix_is_sw_1_1_pressed();
 void keyboard_matrix_init(void);
 void keyboard_matrix_scan(void);
