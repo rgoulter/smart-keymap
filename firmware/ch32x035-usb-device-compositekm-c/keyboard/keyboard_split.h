9ef9febe8fbe05a4455d487a453242ca2d7e4623
Richard Goulter <richard.goulter@gmail.com>
2025-11-02 18:28:17 +0700

    firmware: ch32x: extract keyboard_split function declarations

--- firmware/ch32x035-usb-device-compositekm-c/keyboard/keyboard_split.h
+++ firmware/ch32x035-usb-device-compositekm-c/keyboard/keyboard_split.h
@@ -1,4 +1,7 @@
 #pragma once
 #ifdef KEYBOARD_SPLIT
 #include "generated/keyboard_split.h" // IWYU pragma: export
+#include "smart_keymap.h"
+void keyboard_split_init(void);
+int keyboard_split_write(KeymapInputEvent ev);
 #endif
