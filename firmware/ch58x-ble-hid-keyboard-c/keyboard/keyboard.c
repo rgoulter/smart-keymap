dd441f723821ed2ceff18b419957f0ea053c6e2f
Richard Goulter <richard.goulter@gmail.com>
2025-11-22 21:38:00 +0700

    firmware: ch58x: keyboard: standby/poll

--- firmware/ch58x-ble-hid-keyboard-c/keyboard/keyboard.c
+++ firmware/ch58x-ble-hid-keyboard-c/keyboard/keyboard.c
@@ -1,3 +1,45 @@
+#include "keyboard.h"
+
+#include "CH58x_common.h"
+
+#include "CH58xBLE_LIB.h"
+
+#include "hidkbd.h"
 #include "keyboard_matrix.h"
 
-void keyboard_init(void) { keyboard_matrix_init(); }
+void keyboard_init(void) {
+  keyboard_await_interrupt(); // Start in low-power interrupt mode
+}
+
+void keyboard_start_scanning(void) {
+  PFIC_DisableIRQ(GPIO_A_IRQn);
+  PFIC_DisableIRQ(GPIO_B_IRQn);
+  keyboard_matrix_configure_for_scanning();
+}
+
+void keyboard_await_interrupt(void) {
+  keyboard_matrix_configure_for_interrupt();
+
+  // Clear any pending GPIO interrupts before re-enabling
+  GPIOA_ClearITFlagBit(0xFFFF);
+  GPIOB_ClearITFlagBit(0xFFFF);
+
+  PFIC_EnableIRQ(GPIO_A_IRQn);
+  PFIC_EnableIRQ(GPIO_B_IRQn);
+}
+
+__INTERRUPT
+__HIGH_CODE
+void GPIOA_IRQHandler(void) {
+  // signal main loop to start scanning
+  HidEmu_Wakeup();
+  GPIOA_ClearITFlagBit(0xFFFF);
+}
+
+__INTERRUPT
+__HIGH_CODE
+void GPIOB_IRQHandler(void) {
+  // signal main loop to start scanning
+  HidEmu_Wakeup();
+  GPIOB_ClearITFlagBit(0xFFFF);
+}
--- firmware/ch58x-ble-hid-keyboard-c/keyboard/keyboard.h
+++ firmware/ch58x-ble-hid-keyboard-c/keyboard/keyboard.h
@@ -1,2 +1,4 @@
 #pragma once
 void keyboard_init(void);
+void keyboard_start_scanning(void);
+void keyboard_await_interrupt(void);
