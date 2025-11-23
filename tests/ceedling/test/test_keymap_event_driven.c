ac014432a90c66a57131e55aeebde42dd4eb9d3f
Richard Goulter <richard.goulter@gmail.com>
2025-11-23 13:58:14 +0700

    tests: ceedling: add trivial test demonstrating event driven

---
+++ tests/ceedling/test/test_keymap_event_driven.c
@@ -0,0 +1,47 @@
+#include "unity.h"
+
+#include "smart_keymap.h"
+
+#define KC_A 0x04
+#define KC_B 0x05
+
+void setUp(void) {}
+
+void tearDown(void) {}
+
+// KEYMAP: [C & TH.LCtrl, D & TH.LSft, A, B]
+
+void test_event_driven_key_press(void) {
+  uint8_t expected_report[8] = {0, 0, KC_A, 0, 0, 0, 0, 0};
+  KeymapHidReport report = {};
+  KeymapHidReport* actual_report = &report;
+
+  keymap_init();
+
+  keymap_register_input_after_ms(
+      0,
+      (struct KeymapInputEvent){.event_type = KeymapEventPress, .value = 2},
+      actual_report); // Third key in the keymap is A
+
+  TEST_ASSERT_EQUAL_UINT8_ARRAY(expected_report, actual_report->keyboard, 8);
+}
+
+
+void test_event_driven_key_tap(void) {
+  uint8_t expected_report[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+  KeymapHidReport report = {};
+  KeymapHidReport* actual_report = &report;
+
+  keymap_init();
+
+  keymap_register_input_after_ms(
+      0,
+      (struct KeymapInputEvent){.event_type = KeymapEventPress, .value = 2},
+      actual_report); // Third key in the keymap is A
+  keymap_register_input_after_ms(
+      0,
+      (struct KeymapInputEvent){.event_type = KeymapEventRelease, .value = 2},
+      actual_report); // Third key in the keymap is A
+
+  TEST_ASSERT_EQUAL_UINT8_ARRAY(expected_report, actual_report->keyboard, 8);
+}
