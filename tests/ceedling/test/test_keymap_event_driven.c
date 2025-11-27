de76fda7d2eb0726e2e5b1ca3fbf040ad9282068
Richard Goulter <richard.goulter@gmail.com>
2025-11-27 22:13:12 +0700

    tests: ceedling: add test for event-driven tap

--- tests/ceedling/test/test_keymap_event_driven.c
+++ tests/ceedling/test/test_keymap_event_driven.c
@@ -45,3 +45,40 @@ void test_event_driven_key_tap(void) {
 
   TEST_ASSERT_EQUAL_UINT8_ARRAY(expected_report, actual_report->keyboard, 8);
 }
+
+void test_event_driven_tap_hold_key_tap(void) {
+  uint8_t expected_report[8] = {0, 0, KC_C, 0, 0, 0, 0, 0};
+  KeymapHidReport report = {};
+  KeymapHidReport* actual_report = &report;
+
+  keymap_init();
+
+  // Tap the C & TH.LCtrl
+  uint16_t keymap_index = 0;
+
+  // Press (at time 0)
+  // - schedules event at time 200,
+  // - no key output.
+  {
+    uint32_t actual_next_ev_ms = keymap_register_input_after_ms(
+       0,
+       (struct KeymapInputEvent){.event_type = KeymapEventPress, .value = keymap_index},
+       actual_report);
+
+    TEST_ASSERT_EQUAL_UINT32(200, actual_next_ev_ms);
+    TEST_ASSERT_EQUAL_UINT8_ARRAY(((uint8_t[8]){0, 0, 0, 0, 0, 0, 0, 0}), actual_report->keyboard, 8);
+  }
+
+  // Release (at time 150)
+  // - next ev is 50 ms later (at time 200),
+  // - should have 'C' output.
+  {
+    uint32_t actual_next_ev_ms = keymap_register_input_after_ms(
+       150,
+       (struct KeymapInputEvent){.event_type = KeymapEventRelease, .value = keymap_index},
+       actual_report);
+
+    TEST_ASSERT_EQUAL_UINT32(50, actual_next_ev_ms);
+    TEST_ASSERT_EQUAL_UINT8_ARRAY(((uint8_t[8]){0, 0, KC_C, 0, 0, 0, 0, 0}), actual_report->keyboard, 8);
+  }
+}
