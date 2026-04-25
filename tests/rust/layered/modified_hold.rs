41661ff8787d04fa34c21ef4c74366ff9105012d
Richard Goulter <richard.goulter@gmail.com>
2026-04-25 12:53:16 +0700

    tests: rust: add integration test for modified hold

---
+++ tests/rust/layered/modified_hold.rs
@@ -0,0 +1,60 @@
+use smart_keymap::input;
+use smart_keymap::keymap::ObservedKeymap;
+use smart_keymap_macros::keymap;
+
+#[test]
+fn press_modified_hold_reports_as_modifier() {
+    // Assemble
+    let mut keymap = ObservedKeymap::new(keymap!(
+        r#"
+            let K = import "keys.ncl" in
+            {
+                layers = [
+                    [K.layer_mod.hold 1 & { modifiers = { left_shift = true } }, K.A],
+                    [K.TTTT, K.B],
+                ],
+            }
+        "#
+    ));
+
+    // Act
+    keymap.handle_input(input::Event::Press { keymap_index: 0 });
+
+    keymap.tick_until_no_scheduled_events();
+
+    // Assert
+    let expected_reports: &[[u8; 8]] = &[[0, 0, 0, 0, 0, 0, 0, 0], [0x02, 0, 0, 0, 0, 0, 0, 0]];
+    let actual_reports = keymap.distinct_reports();
+    assert_eq!(expected_reports, actual_reports.reports());
+}
+
+#[test]
+fn press_modified_hold_modifies_layer() {
+    // Assemble
+    let mut keymap = ObservedKeymap::new(keymap!(
+        r#"
+            let K = import "keys.ncl" in
+            {
+                layers = [
+                    [K.layer_mod.hold 1 & { modifiers = { left_shift = true } }, K.A],
+                    [K.TTTT, K.B],
+                ],
+            }
+        "#
+    ));
+
+    // Act
+    keymap.handle_input(input::Event::Press { keymap_index: 0 });
+    keymap.handle_input(input::Event::Press { keymap_index: 1 });
+
+    keymap.tick_until_no_scheduled_events();
+
+    // Assert
+    let expected_reports: &[[u8; 8]] = &[
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0x02, 0, 0, 0, 0, 0, 0, 0],
+        [0x02, 0, 0x05, 0, 0, 0, 0, 0],
+    ];
+    let actual_reports = keymap.distinct_reports();
+    assert_eq!(expected_reports, actual_reports.reports());
+}
