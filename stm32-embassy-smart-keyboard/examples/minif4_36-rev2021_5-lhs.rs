721b975d7f2403e5840381435875120064efdeae
Richard Goulter <richard.goulter@gmail.com>
2026-07-06 23:28:38 +0700

    stm32-embassy-smart-keyboard: fix smart_keymap import path
    Use smart_keymap::key directly; keyberon-smart-keyboard does not
    re-export the smart_keymap crate.

--- stm32-embassy-smart-keyboard/examples/minif4_36-rev2021_5-lhs.rs
+++ stm32-embassy-smart-keyboard/examples/minif4_36-rev2021_5-lhs.rs
@@ -26,7 +26,7 @@ use usbd_hid::descriptor::{
 use keyberon_smart_keyboard::input::smart_keymap::keymap_index_of;
 use keyberon_smart_keyboard::input::smart_keymap::KeyboardBackend;
 use keyberon_smart_keyboard::input::MatrixScanner;
-use keyberon_smart_keyboard::smart_keymap::key;
+use smart_keymap::key;
 use keyberon_smart_keyboard::split::BackendMessage;
 
 use smart_keymap::input::Event;
