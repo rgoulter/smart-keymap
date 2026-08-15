b6854c0225fa6233d9ad32179d3b8998c9604381
Richard Goulter <richard.goulter@gmail.com>
2026-08-15 10:55:48 +0700

    test: ceedling suite for split 3x5+3 thumb keys
    Cover tap and hold for each thumb, and tap/hold for the two
    inner-pair chords (Esc+Space, Enter+Backspace).

--- tests/ceedling/test/support/hid_keycodes.h
+++ tests/ceedling/test/support/hid_keycodes.h
@@ -5,6 +5,15 @@
 #define KC_B 0x05
 #define KC_C 0x06
 #define KC_D 0x07
+#define KC_N1 0x1E
+#define KC_RETURN 0x28
+#define KC_ESCAPE 0x29
+#define KC_BACKSPACE 0x2A
+#define KC_TAB 0x2B
+#define KC_SPACE 0x2C
+#define KC_F1 0x3A
+#define KC_DELETE 0x4C
+#define KC_LEFT 0x50
 
 // Modifier bits in KeymapHidReport.keyboard[0]
 #define MOD_LCTL 0x01
